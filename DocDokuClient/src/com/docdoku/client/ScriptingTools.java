cdce3bb15a9b1db3e7d80bdd74404d06c86f2d01
Florent Garin <florent.garin@docdoku.com>
2010-10-28 10:50:10 +0000

    add method to retrieve the fileManagerServer

--- DocDokuClient/src/com/docdoku/client/ScriptingTools.java
+++ DocDokuClient/src/com/docdoku/client/ScriptingTools.java
@@ -21,9 +21,12 @@
 package com.docdoku.client;
 
 import com.docdoku.core.ICommandWS;
+import com.sun.xml.ws.developer.JAXWSProperties;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.soap.MTOMFeature;
 
 /**
  *
@@ -37,7 +40,7 @@ public class ScriptingTools {
     
     private ScriptingTools(){}
     
-    public static ICommandWS logToServer(String url, String login, String password, String workspace) throws MalformedURLException, Exception{
+    public static ICommandWS createCommandServer(String url, String login, String password) throws MalformedURLException, Exception{
         CommandService service = new CommandService(new URL(url),new javax.xml.namespace.QName("http://server.docdoku.com/", "CommandBeanService"));
         ICommandWS port= service.getPort(ICommandWS.class);
         ((BindingProvider)port).getRequestContext().put( BindingProvider.USERNAME_PROPERTY, login);
@@ -46,8 +49,23 @@ public class ScriptingTools {
         return port;
     }
     
-    public static ICommandWS logToServer(String login, String password, String workspace) throws MalformedURLException, Exception{
-        return logToServer(DEFAULT_WSDL_LOCATION,login,password,workspace);
+    public static com.docdoku.client.ws.proxies.uploaddownloadservice.UploadDownload createFileManagerServer(String url, String login, String password) throws MalformedURLException{       
+        MTOMFeature feature = new MTOMFeature();
+        com.docdoku.client.ws.proxies.uploaddownloadservice.UploadDownloadService service = new com.docdoku.client.ws.proxies.uploaddownloadservice.UploadDownloadService(new URL(url),new javax.xml.namespace.QName("http://http.server.docdoku.com/", "UploadDownloadService"));
+        com.docdoku.client.ws.proxies.uploaddownloadservice.UploadDownload proxy = service.getUploadDownloadPort(feature);        
+        Map context = ((BindingProvider)proxy).getRequestContext();
+        context.put(JAXWSProperties.HTTP_CLIENT_STREAMING_CHUNK_SIZE, 8192);
+        context.put( BindingProvider.USERNAME_PROPERTY, login);
+        context.put( BindingProvider.PASSWORD_PROPERTY, password);
+        return proxy;
+    }
+
+    public static ICommandWS createCommandServer(String login, String password) throws MalformedURLException, Exception{
+        return createCommandServer(DEFAULT_WSDL_LOCATION,login,password);
+    }
+
+    public static com.docdoku.client.ws.proxies.uploaddownloadservice.UploadDownload createFileManagerServer(String login, String password) throws MalformedURLException, Exception{
+        return createFileManagerServer(DEFAULT_WSDL_LOCATION,login,password);
     }
     
 }
