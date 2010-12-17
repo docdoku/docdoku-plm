ffb42f714ecaf7235ab9d1eb4ab868907cd98f50
Florent Garin <florent.garin@docdoku.com>
2010-12-17 23:06:28 +0000

    add accidentally removed files

--- DocDoku/DocDoku-war/src/java/com/docdoku/gwt/client/ui/widget/util/NotEmptyChecker.java
+++ DocDoku/DocDoku-war/src/java/com/docdoku/gwt/client/ui/widget/util/NotEmptyChecker.java
@@ -1,7 +1,29 @@
-package fr.senioriales.stocks.gwt.client.ui.widget.util;
+/*
+ * NotEmptyChecker.java
+ * 
+ * Copyright (c) 2009 Docdoku. All rights reserved.
+ * 
+ * This file is part of Docdoku.
+ * 
+ * Docdoku is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Docdoku is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Docdoku.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.docdoku.gwt.client.ui.widget.util;
 
 /**
  * A checker that accepts input if it is not empty
+ * @author Emmanuel Nhan <emmanuel.nhan@insa-lyon.fr>
  */
 public class NotEmptyChecker implements DocdokuChecker{
 
