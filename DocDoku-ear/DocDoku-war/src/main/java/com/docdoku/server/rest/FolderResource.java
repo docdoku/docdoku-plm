25b4990f7b457d34852f6fe0b45c1e9788a16804
Yassine Belouad <yassinebelouad@gmail.com>
2012-02-10 16:28:24 +0000

    Added json WS to rename a folder

--- DocDoku-ear/DocDoku-war/src/main/java/com/docdoku/server/rest/FolderResource.java
+++ DocDoku-ear/DocDoku-war/src/main/java/com/docdoku/server/rest/FolderResource.java
@@ -17,28 +17,24 @@
  * You should have received a copy of the GNU General Public License  
  * along with DocDoku.  If not, see <http://www.gnu.org/licenses/>.  
  */
-
 package com.docdoku.server.rest;
 
 import com.docdoku.core.document.DocumentMasterKey;
 import com.docdoku.core.document.Folder;
 import com.docdoku.core.security.UserGroupMapping;
-import com.docdoku.core.services.ICommandLocal;
-import com.docdoku.core.services.IUserManagerLocal;
+import com.docdoku.core.services.*;
 import com.docdoku.server.rest.dto.FolderDTO;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import org.dozer.DozerBeanMapperSingletonWrapper;
 import org.dozer.Mapper;
 
@@ -50,40 +46,38 @@ public class FolderResource {
 
     @EJB
     private ICommandLocal commandService;
-
     @EJB
     private IUserManagerLocal userManager;
-    
-    
     @Context
     private UriInfo context;
-    
     private Mapper mapper;
 
     public FolderResource() {
     }
 
     @PostConstruct
-    public void init(){
+    public void init() {
         mapper = DozerBeanMapperSingletonWrapper.getInstance();
     }
-        
+
     /**
      * Retrieves representation of an instance of FolderResource
+     *
      * @param parent folder path
      * @return the array of sub-folders
-     */    
+     */
     @GET
     @Path("{completePath:.*}")
     @Produces("application/json;charset=UTF-8")
     public FolderDTO[] getJson(@PathParam("completePath") String completePath) {
         try {
-            completePath=Tools.stripTrailingSlash(completePath);
+            completePath = Tools.stripTrailingSlash(completePath);
             String[] folderNames = commandService.getFolders(completePath);
             FolderDTO[] folderDtos = new FolderDTO[folderNames.length];
-            for(int i = 0; i<folderNames.length;i++)
-                folderDtos[i]= new FolderDTO(completePath,folderNames[i]);
-           
+            for (int i = 0; i < folderNames.length; i++) {
+                folderDtos[i] = new FolderDTO(completePath, folderNames[i]);
+            }
+
             return folderDtos;
         } catch (com.docdoku.core.services.ApplicationException ex) {
             throw new RESTException(ex.toString(), ex.getMessage());
@@ -92,23 +86,55 @@ public class FolderResource {
 
     /**
      * PUT method for updating or creating an instance of FolderResource
+     *
      * @param parent folder path and name of the folder to create
      */
     @PUT
     @Path("{completePath:.*}")
     public void putJson(@PathParam("completePath") String completePath) {
         try {
-            int lastSlash=completePath.lastIndexOf('/');
-            String parentFolder = completePath.substring(0,lastSlash);
-            String folderName = completePath.substring(lastSlash+1, completePath.length());
+            int lastSlash = completePath.lastIndexOf('/');
+            String parentFolder = completePath.substring(0, lastSlash);
+            String folderName = completePath.substring(lastSlash + 1, completePath.length());
             commandService.createFolder(parentFolder, folderName);
         } catch (com.docdoku.core.services.ApplicationException ex) {
             throw new RESTException(ex.toString(), ex.getMessage());
         }
     }
+
     
+    /* JSON WS to rename a folder */
+    
+//    @POST
+//    @Path("{completePath:.*}")
+//    @Consumes("application/json;charset=UTF-8")
+//    public Response renameFolder(@PathParam("completePath") String completePath, FolderDTO folderDTO) {
+//        try {
+//            
+//            completePath = Tools.stripTrailingSlash(completePath);
+//            int lastSlash = completePath.lastIndexOf('/');
+//            String newFolderCompletePath = folderDTO.getCompletePath();
+//            String newFolderName = newFolderCompletePath.substring(lastSlash + 1, newFolderCompletePath.length());
+//
+//            //TODO
+//            commandService.renameFolder(completePath, newFolderName);
+//
+//            return Response.status(Response.Status.OK).build();
+//
+//        } catch (WorkspaceNotFoundException ex) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        } catch (FolderNotFoundException ex) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        } catch (UserNotFoundException ex) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        } catch (UserNotActiveException ex) {
+//            return Response.status(Response.Status.NOT_FOUND).build();
+//        }
+//    }
+
     /**
      * DELETE method for deleting an instance of FolderResource
+     *
      * @param parent folder path
      * @return the array of the documents that have also been deleted
      */
@@ -116,14 +142,11 @@ public class FolderResource {
     @Path("{completePath:.*}")
     @Produces("application/json;charset=UTF-8")
     public DocumentMasterKey[] deleteJson(@PathParam("completePath") String completePath) {
-        try {       
-            completePath=Tools.stripTrailingSlash(completePath);
+        try {
+            completePath = Tools.stripTrailingSlash(completePath);
             return commandService.deleteFolder(completePath);
         } catch (com.docdoku.core.services.ApplicationException ex) {
             throw new RESTException(ex.toString(), ex.getMessage());
         }
     }
-    
-    
-    
 }
