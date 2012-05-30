f5a401c7c64aa4b3dc74207bc1179ce8de286495
Antoine Cezar <antoine.cezar@gmail.com>
2012-05-30 12:47:32 +0000

    Merge branch 'file_upload'

--- DocDoku-ear/DocDoku-war/src/main/webapp/js/views/document_list_item.js
+++ DocDoku-ear/DocDoku-war/src/main/webapp/js/views/document_list_item.js
@@ -20,6 +20,7 @@ define([
 		},
 		modelToJSON: function () {
 			var data = this.model.toJSON();
+			data.lastIteration = this.model.lastIteration.toJSON();
 
 			// Format dates
 			if (data.lastIteration && data.lastIteration.creationDate) {
