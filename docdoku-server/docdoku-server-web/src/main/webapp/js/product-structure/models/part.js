59ab24934da500ec6702f6832ef25c9933c86567
Julien Maffre <jumaffre@gmail.com>
2012-08-02 16:46:08 +0200

    fix indentation

--- docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/models/part.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/models/part.js
@@ -1,65 +1,67 @@
 window.Part = Backbone.Model.extend({
-        defaults: {
-            "name":  null,
-            "number":     null,
-            "version":    null,
-            "description":  null,
-            "instances":     null,
-            "files":    null,
-            "components": null,
-            "isNode":false
-        },
 
-        idAttribute: "number",
+    defaults: {
+        "name":  null,
+        "number":     null,
+        "version":    null,
+        "description":  null,
+        "instances":     null,
+        "files":    null,
+        "components": null,
+        "isNode":false
+    },
 
-        initialize : function(){
-            this.className = "Part";
-            if (this.getComponents().length>0) {
-                this.set('isNode', true);
-            }
-        },
+    idAttribute: "number",
 
-        getName : function() {
-            return this.get('name');
-        },
-
-        getNumber : function() {
-            return this.get('number');
-        },
-
-        getVersion : function() {
-            return this.get('version');
-        },
-
-        getDescription : function() {
-            return this.get('description');
-        },
-
-        getInstances : function() {
-            return this.get('instances');
-        },
-
-        getFiles : function() {
-            return this.get('files');
-        },
-
-        getComponents : function() {
-            return this.get('components');
-        },
-
-        getWorkspaceId : function() {
-            return this.get('worksapceId');
-        },
-
-        getIteration : function() {
-            return this.get('iteration');
-        },
-
-        getStandardPart : function() {
-            return this.get('standarPart');
-        },
-
-        isNode: function() {
-            return this.get('isNode');
+    initialize : function(){
+        this.className = "Part";
+        if (this.getComponents().length>0) {
+            this.set('isNode', true);
         }
-    });
+    },
+
+    getName : function() {
+        return this.get('name');
+    },
+
+    getNumber : function() {
+        return this.get('number');
+    },
+
+    getVersion : function() {
+        return this.get('version');
+    },
+
+    getDescription : function() {
+        return this.get('description');
+    },
+
+    getInstances : function() {
+        return this.get('instances');
+    },
+
+    getFiles : function() {
+        return this.get('files');
+    },
+
+    getComponents : function() {
+        return this.get('components');
+    },
+
+    getWorkspaceId : function() {
+        return this.get('worksapceId');
+    },
+
+    getIteration : function() {
+        return this.get('iteration');
+    },
+
+    getStandardPart : function() {
+        return this.get('standardPart');
+    },
+
+    isNode: function() {
+        return this.get('isNode');
+    }
+
+});
