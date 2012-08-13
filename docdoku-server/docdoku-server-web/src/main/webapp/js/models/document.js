9c78e1c2578d9a48fd97bd3bc7404d16b380feb6
Florent Garin <florent.garin@docdoku.com>
2012-08-13 16:23:30 +0200

    merge ged

--- docdoku-server/docdoku-server-web/src/main/webapp/js/models/document.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/models/document.js
@@ -10,19 +10,33 @@ define([
 	var Document = Backbone.Model.extend({
 		initialize: function () {
             var doc = this;
+
             this.className = "Document";
 
-            var idSplit = this.id.split("-");
-            kumo.assert(idSplit.length == 2, "the id should be in the form document-VERSION");
-            this.set("docKey", this.id);
-            this.set("name", idSplit[0]);
-            this.set("version", idSplit[1]);
-            this.set("workspace", APP_CONFIG.workspaceId);
+            if (kumo.isNotEmpty(this.id)){
+                var index = this.id.lastIndexOf("-");
+                if (index >0 ){
+                    this.set("docKey", this.id);
 
-            _.each(this.iterations.models, function(iteration){
-                kumo.assert(iteration.className=="DocumentIteration");
-                iteration.set("document",  doc);
-            });
+                    //the name may contain some '-'
+                    this.set("name", this.id.substring(0, index));
+                    this.set("version", this.id.substring(index+1, this.id.length));
+
+                    //setting doc object to iterations
+                    _.each(this.iterations.models, function(iteration){
+                        kumo.assert(iteration.className=="DocumentIteration");
+                        iteration.set("document",  doc);
+                    });
+
+
+                }else{
+                    console.error("Document id "+this.id+" should contain '-'");
+                }
+            }
+
+
+            //this.set("workspace", APP_CONFIG.workspaceId);
+
 
 			_.bindAll(this);
 		},
@@ -43,7 +57,14 @@ define([
 		},
 
         getDocKey : function(){
-            return this.get("docKey");
+            if (kumo.isEmpty(this.get("docKey"))){
+                //looks like being a problem just after the creation of a document
+                console.error("Creation of a document ; docKey not set, sending back the id");
+                return this.id;
+            }else{
+                return this.get("docKey");
+            }
+
         },
 
         getName : function(){
@@ -55,12 +76,22 @@ define([
         },
 
         getWorkspace : function(){
-          return this.get("workspace");
+          return this.get("workspaceId");
         },
 
         getUrl : function(){
           return this.url();
         },
+
+        getLastIteration : function(){
+           return this.get("lastIteration");
+        },
+
+        getIterations : function(){
+          return this.get("documentIterations");
+        },
+
+
 
         /**
          * @deprecated ; use this.getUrl()
@@ -71,7 +102,7 @@ define([
 			if (this.get("id")) {
 				return  "/api/workspaces/" + this.getWorkspace()+ "/documents/"+this.getDocKey();
 			} else if (this.collection) {
-                console.log("We are here in document.url with no id ;");
+                console.log("Creation of a document ; we send back the url of the folder collection");
 				return this.collection.url;
 			}
 		},
