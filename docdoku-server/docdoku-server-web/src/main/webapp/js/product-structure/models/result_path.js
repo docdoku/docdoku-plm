2f191e94af662e83e5b93116f7ef7ef6b2eb73fd
Julien Maffre <jumaffre@gmail.com>
2012-12-10 14:36:11 +0100

    fix search, and add root element in search results

--- docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/models/result_path.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/models/result_path.js
@@ -3,14 +3,16 @@ define(function () {
     var ResultPath = Backbone.Model.extend({
 
         contains: function(partUsageLinkId) {
-            return _.indexOf(this.partUsageLinks, partUsageLinkId);
+            return _.indexOf(this.partUsageLinks, partUsageLinkId) != -1;
         },
 
         parse: function(response) {
             if (response) {
+                this.partUsageLinks
                 this.partUsageLinks = _.map(response.split('-'), function(partUsageLinkIdString) {
                     return parseInt(partUsageLinkIdString, 10);
                 });
+                this.partUsageLinks.unshift(-1);
             }
         },
 
