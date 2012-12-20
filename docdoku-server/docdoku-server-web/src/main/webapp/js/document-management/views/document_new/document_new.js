6ff6a837e4c21d1f247f65f91638e32c48e6cacd
Julien Maffre <jumaffre@gmail.com>
2012-12-20 15:08:46 +0100

    format code

--- docdoku-server/docdoku-server-web/src/main/webapp/js/document-management/views/document_new/document_new.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/document-management/views/document_new/document_new.js
@@ -13,82 +13,85 @@ define([
 ) {
 	var DocumentNewView = ModalView.extend({
 
-		template: Mustache.compile(template),
+        template: Mustache.compile(template),
 
-		initialize: function () {
-			ModalView.prototype.initialize.apply(this, arguments);
-			this.events["submit #form-" + this.cid] = "onSubmitForm";
-		},
+        initialize: function() {
+            ModalView.prototype.initialize.apply(this, arguments);
+            this.events["submit #form-" + this.cid] = "onSubmitForm";
+        },
 
-		rendered: function () {
-			this.attributesView = this.addSubView(
-				new DocumentNewAttributesView({
-					el: "#tab-attributes-" + this.cid
-				})
-			);
-			this.attributesView.render();
+        rendered: function() {
+            this.attributesView = this.addSubView(
+                new DocumentNewAttributesView({
+                    el: "#tab-attributes-" + this.cid
+                })
+            );
+            this.attributesView.render();
 
-			this.templatesView = this.addSubView(
-				new DocumentNewTemplateListView({
-					el: "#templates-" + this.cid,
-					attributesView: this.attributesView
-				})
-			);
-			this.templatesView.collection.fetch();
+            this.templatesView = this.addSubView(
+                new DocumentNewTemplateListView({
+                    el: "#templates-" + this.cid,
+                    attributesView: this.attributesView
+                })
+            );
+            this.templatesView.collection.fetch();
 
-			this.workflowsView = this.addSubView(
-				new DocumentNewWorkflowListView({
-					el: "#workflows-" + this.cid
-				})
-			);
-			this.workflowsView.collection.fetch();
-		},
+            this.workflowsView = this.addSubView(
+                new DocumentNewWorkflowListView({
+                    el: "#workflows-" + this.cid
+                })
+            );
+            this.workflowsView.collection.fetch();
+        },
 
-        onSubmitForm: function () {
-			var reference = $("#form-" + this.cid + " .reference").val();
+        onSubmitForm: function() {
+            var reference = $("#form-" + this.cid + " .reference").val();
 
             if (reference) {
-				var workflow = this.workflowsView.selected();
-				var data = {
-					reference: reference,
-					title: $("#form-" + this.cid + " .title").val(),
-					description: $("#form-" + this.cid + " .description").val(),
-					workflowModelId: workflow ? workflow.get("id") : null
-				};
+                var workflow = this.workflowsView.selected();
+                var data = {
+                    reference: reference,
+                    title: $("#form-" + this.cid + " .title").val(),
+                    description: $("#form-" + this.cid + " .description").val(),
+                    workflowModelId: workflow ? workflow.get("id") : null
+                };
 
-				this.collection.create(data, {
-					success: this.success,
-					error: this.error
-				});
-			}
+                this.collection.create(data, {
+                    success: this.success,
+                    error: this.error
+                });
+            }
 
-			return false;
-		},
+            return false;
+        },
 
-		success: function (model, response) {
-			var that = this;
-			model.lastIteration.save({
-				instanceAttributes: this.attributesView.collection.toJSON()
-			}, {
-				success: function () {
-					that.hide();
-				},
-				error: this.error
-			});
-			model.fetch();
-		},
+        success: function(model, response) {
+            var that = this;
+            model.lastIteration.save({
+                instanceAttributes: this.attributesView.collection.toJSON()
+            }, {
+                success: function() {
+                    that.hide();
+                },
+                error: this.error
+            });
+            model.fetch();
+        },
 
-		error: function (model, error) {
-			this.collection.remove(model);
-			if (error.responseText) {
-				this.alert({
-					type: "error",
-					message: error.responseText
-				});
-			} else {
-				console.error(error);
-			}
-		}
-	});
-	return DocumentNewView;
+        error: function(model, error) {
+            this.collection.remove(model);
+            if (error.responseText) {
+                this.alert({
+                    type: "error",
+                    message: error.responseText
+                });
+            } else {
+                console.error(error);
+            }
+        }
+
+    });
+
+    return DocumentNewView;
+
 });
