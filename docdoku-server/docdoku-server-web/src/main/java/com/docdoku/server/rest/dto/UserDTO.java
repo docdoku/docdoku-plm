317a9059f7118fe21046467a213d2be492a5dba0
Florent Garin <florent.garin@docdoku.com>
2013-01-14 14:41:17 +0100

    change copyright header

--- docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/dto/UserDTO.java
+++ docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/dto/UserDTO.java
@@ -2,20 +2,20 @@
  * DocDoku, Professional Open Source
  * Copyright 2006 - 2013 DocDoku SARL
  *
- * This file is part of DocDoku.
+ * This file is part of DocDokuPLM.
  *
- * DocDoku is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DocDoku is distributed in the hope that it will be useful,  
+ * DocDokuPLM is distributed in the hope that it will be useful,  
  * but WITHOUT ANY WARRANTY; without even the implied warranty of  
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
- * GNU General Public License for more details.  
+ * GNU Affero General Public License for more details.  
  *  
- * You should have received a copy of the GNU General Public License  
- * along with DocDoku.  If not, see <http://www.gnu.org/licenses/>.  
+ * You should have received a copy of the GNU Affero General Public License  
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.  
  */
 
 package com.docdoku.server.rest.dto;
