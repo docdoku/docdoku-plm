615f3a188230e646c01e232aadb296a48a2cc39e
Florent Garin <florent.garin@docdoku.com>
2013-01-16 17:06:58 +0100

    fix "guide menu" refresh on rich client

--- docdoku-client/src/main/java/com/docdoku/client/ui/ExplorerFrame.java
+++ docdoku-client/src/main/java/com/docdoku/client/ui/ExplorerFrame.java
@@ -241,6 +241,7 @@ public class ExplorerFrame extends JFrame implements HasElementSelectedListeners
     public void showShortcutsPanel(){
         mFolderTree.setSelectionPath(null);
         setContentPane(mShortcutsPanel);
+        revalidate();
     }
 
     private void createListener() {
