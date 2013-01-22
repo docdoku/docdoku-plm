a76c6d3e2681d419f3ce273ad888341357630207
nicolas <nicolas.ruault@docdoku.com>
2013-01-22 16:30:39 +0100

    modifications affichage vidéo

--- docdoku-server/docdoku-server-web/src/main/webapp/js/switchPlayer.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/switchPlayer.js
@@ -1,4 +1,4 @@
-function switchMenu(obj, btn, context) {
+function switchMenu(obj, btn, context, path) {
     var el = document.getElementById(obj);
                 
     if ( el.style.display != 'none' ) {
@@ -15,3 +15,20 @@ function openFullScreen(strUrl, strWindowName) {
     window.open(strUrl, strWindowName, "");
 }
 
+/*function checkExtention(verifExt,path) {
+    var extension=path.substring(path.lastIndexOf(".")+1, path.length);
+
+    if (extension==verifExt)
+        return true;
+    else
+        return false;
+}
+
+function changeExtention(path) {
+    var name=path.split(".");
+    var file=name[0];
+    var extension=name[1];
+    var newExt=name[0] + '.ogg';
+
+    return (newExt);
+}*/
