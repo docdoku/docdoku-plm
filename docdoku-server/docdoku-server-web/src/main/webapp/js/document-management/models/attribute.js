b7d2af5a2d275edec7d85b2db83a296f8a210325
Julien Maffre <jumaffre@gmail.com>
2013-01-24 18:46:56 +0100

    remove validate method

--- docdoku-server/docdoku-server-web/src/main/webapp/js/document-management/models/attribute.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/document-management/models/attribute.js
@@ -18,47 +18,6 @@ define([
 
         toString:function () {
             return this.getName() + ":" + this.getValue() + "(" + this.getType() + ") ";
-        },
-
-        //from backbone Doc : If the attributes are valid, don't return anything from validate
-        validate_:function () {
-
-            try{
-                var value = this.getValue();
-                var ok = true;
-                switch (this.getType()) {
-
-                    case Attribute.types.NUMBER :
-                    case Attribute.types.DATE :
-                        value = parseFloat(value);
-                        this.set({value : value}, {silent:true});//fixing missed conversion
-                        ok = (_.isNumber(value) && !_.isNaN(value));
-                        break;
-
-                    case Attribute.types.BOOLEAN :
-                        //value=Boolean(value);
-                        value = (value=="true") ? true : false;
-                        this.set({value : value}, {silent:true});//fixing missed conversion
-                        ok = (value === true || value === false) && typeof(value) == "boolean";
-                        break;
-
-                    case Attribute.types.TEXT :
-                    case Attribute.types.URL :
-                        ok = typeof(value) == "string" && value !="";
-
-                        break;
-                }
-            }catch (e){
-                console.error("Unexpected fail during validation "+e);
-                return i18n.VALIDATION_FAILED_FOR+this.getName();
-            }
-
-            if (!ok){
-                console.error ("validation error :"+this.getName()+" : "+this.getValue());
-                return i18n.VALIDATION_FAILED_FOR+this.getName()+" : "+this.getValue();
-
-            }
-
         }
 
     });
