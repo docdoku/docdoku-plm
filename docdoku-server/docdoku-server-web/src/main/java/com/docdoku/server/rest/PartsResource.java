36fa8347227bd0671ac0520d7cfc59e8a54c0285
jmaffre <jumaffre@gmail.com>
2013-01-28 17:44:54 +0100

    add a service to find part masters by their number

---
+++ docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/PartsResource.java
@@ -0,0 +1,69 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2013 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU Affero General Public License for more details.  
+ *  
+ * You should have received a copy of the GNU Affero General Public License  
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.  
+ */
+package com.docdoku.server.rest;
+
+import com.docdoku.core.product.PartMaster;
+import com.docdoku.core.security.UserGroupMapping;
+import com.docdoku.core.services.IProductManagerLocal;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import org.dozer.DozerBeanMapperSingletonWrapper;
+import org.dozer.Mapper;
+
+@Stateless
+@DeclareRoles(UserGroupMapping.REGULAR_USER_ROLE_ID)
+@RolesAllowed(UserGroupMapping.REGULAR_USER_ROLE_ID)
+public class PartsResource {
+
+    @EJB
+    private IProductManagerLocal productService;
+
+    private Mapper mapper;
+
+    public PartsResource() {
+    }
+
+    @PostConstruct
+    public void init() {
+        mapper = DozerBeanMapperSingletonWrapper.getInstance();
+    }
+
+    @GET
+    @Produces("application/json;charset=UTF-8")
+    public String[] searchPartNumbers(@PathParam("workspaceId") String workspaceId, @QueryParam("q") String q) {
+        try {
+            List<PartMaster> partMasters = productService.findPartMasters(Tools.stripTrailingSlash(workspaceId), q);
+            String[] partNumbers = new String[partMasters.size()];
+            for (int i = 0; i < partMasters.size(); i++) {
+                partNumbers[i] = partMasters.get(i).getNumber();
+            }
+            return partNumbers;
+        } catch (com.docdoku.core.services.ApplicationException ex) {
+            throw new RestApiException(ex.toString(), ex.getMessage());
+        }
+    }
+
+}
