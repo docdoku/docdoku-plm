982fd3db138526ed3f4f1bb451cc46068f07f744
Florent Garin <florent.garin@docdoku.com>
2013-02-05 16:47:22 +0100

    structure CLI API

---
+++ docdoku-cli/src/main/java/com/docdoku/cli/commands/AbstractCommandLine.java
@@ -0,0 +1,37 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2013 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.docdoku.cli.commands;
+
+
+import org.kohsuke.args4j.Option;
+
+public abstract class AbstractCommandLine implements CommandLine{
+
+
+    @Option(name="-p", aliases = "--password", metaVar = "<password>", usage="")
+    protected String password;
+
+    @Option(name="-u", aliases = "--user", metaVar = "<user>", usage="")
+    protected String login;
+
+    @Override
+    public abstract void exec();
+}
