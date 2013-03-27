3c5babc9fb5aa19406bcdda1c1a2e29ddfe6779c
etiennemonier <etienne.monier@docdoku.net>
2013-03-27 17:33:33 +0100

    create new module for SCORM file

---
+++ docdoku-server/docdoku-server-scorm/src/main/java/com/docdoku/server/resourcegetters/ScormResourceGetterImpl.java
@@ -0,0 +1,36 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2013 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.docdoku.server.resourcegetters;
+
+import java.io.File;
+
+@ScormResourceGetter
+public class ScormResourceGetterImpl implements DocumentResourceGetter {
+
+    @Override
+    public File getResource(String fileName) throws Exception {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean canGetResource(String fileName) {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
