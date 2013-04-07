dd7c8105e9bfec1073ba67947f77968005fb7905
Julien Maffre <jumaffre@gmail.com>
2013-04-07 17:03:51 +0200

    Move plugins DocumentViewer, DocumentPostUploader & DocumentResourceGetter in docdoku-server-web Rename visualizers by viewers

---
+++ docdoku-server/docdoku-server-viewer-multimedia/src/main/java/com/docdoku/server/viewers/MultimediaViewerImpl.java
@@ -0,0 +1,48 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2013 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.docdoku.server.viewers;
+
+import com.docdoku.core.util.FileIO;
+
+import javax.activation.FileTypeMap;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+public class MultimediaViewerImpl implements DocumentViewer {
+    @Override
+    public boolean canGetResourceForViewer(File file, HttpServletRequest pRequest) {
+        return FileIO.isAVFile(file.getName());
+    }
+
+    @Override
+    public File getFileForViewer(HttpServletRequest pRequest, HttpServletResponse pResponse, ServletContext servletContext, File dataFile) throws Exception {
+        String contentType = FileTypeMap.getDefaultFileTypeMap().getContentType(dataFile);
+        pResponse.setContentType(contentType);
+        return dataFile;
+    }
+
+    @Override
+    public boolean canVisualize(String fileName) {
+        return FileIO.isAVFile(fileName);
+    }
+
+}
