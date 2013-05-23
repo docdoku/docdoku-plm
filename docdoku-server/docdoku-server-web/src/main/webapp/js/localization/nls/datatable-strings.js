a4a7f009062fc3bea0d626ec13e0022e0b40fe65
morgan <morgan.guimard@docdoku.com>
2013-05-23 09:45:45 +0200

    added missing translations for datatables

--- docdoku-server/docdoku-server-web/src/main/webapp/js/localization/nls/datatable-strings.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/localization/nls/datatable-strings.js
@@ -1,6 +1,8 @@
 define({
     root: {
-       FILTER:"Filter table"
+       FILTER:"Filter table",
+       NO_DATA:"No data available in table",
+       NO_FILTERED_DATA:"No matching records found"
     },
     "fr": true
 });
--- docdoku-server/docdoku-server-web/src/main/webapp/js/product-management/views/part_list.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/product-management/views/part_list.js
@@ -189,7 +189,9 @@ define([
                 bDestroy:true,
                 iDisplayLength:-1,
                 oLanguage:{
-                    sSearch: "<i class='icon-search'></i>"
+                    sSearch: "<i class='icon-search'></i>",
+                    sEmptyTable:i18nDt.NO_DATA,
+                    sZeroRecords:i18nDt.NO_FILTERED_DATA
                 },
                 sDom : 'ft',
                 aoColumnDefs: [
