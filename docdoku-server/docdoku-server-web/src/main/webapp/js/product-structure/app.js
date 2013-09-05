7d258acc5a9044d3c28a448590428d592bd550a9
morgan <morgan.guimard@docdoku.com>
2013-09-05 10:14:35 +0200

    feature-optim-dmu-base

--- docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/app.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/app.js
@@ -14,7 +14,7 @@ define(
         "views/control_layers_view",
         "views/control_options_view",
         "views/control_explode_view",
-        "views/control_cutplan_view",
+        //"views/control_cutplan_view",
         "views/baseline_select_view",
         "dmu/SceneManager",
         "text!templates/content.html",
@@ -32,7 +32,7 @@ define(
                  ControlLayersView,
                  ControlOptionsView,
                  ControlExplodeView,
-                 ControlCutPlanView,
+                 //ControlCutPlanView,
                  BaselineSelectView,
                  SceneManager,
                  template,
@@ -80,7 +80,7 @@ define(
             this.$ControlsContainer.append(new ControlModesView().render().$el);
             this.$ControlsContainer.append(new ControlOptionsView().render().$el);
             this.$ControlsContainer.append(new ControlExplodeView().render().$el);
-            this.$ControlsContainer.append(new ControlCutPlanView().render().$el);
+            //this.$ControlsContainer.append(new ControlCutPlanView().render().$el);
             this.$ControlsContainer.append(new ControlMarkersView().render().$el);
             this.$ControlsContainer.append(new ControlLayersView().render().$el);
 
