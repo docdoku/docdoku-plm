5b93fcad3c8d316ffb555687f4681063575ec3b7
Morgan Guimard <morgan.guimard@docdoku.com>
2014-05-17 13:58:49 +0200

    wip user guide

--- docdoku-user-guides/Gruntfile.js
+++ docdoku-user-guides/Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 9002,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
