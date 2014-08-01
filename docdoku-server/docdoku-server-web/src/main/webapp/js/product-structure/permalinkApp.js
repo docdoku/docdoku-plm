2d70dac6df6d8a7473d5edc664d436500c7aca39
Arthurin <arthurfrin@gmail.com>
2014-08-01 17:30:22 +0200

    PermalinkApp : create a scene from scratch.

--- docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/permalinkApp.js
+++ docdoku-server/docdoku-server-web/src/main/webapp/js/product-structure/permalinkApp.js
@@ -20,33 +20,74 @@ var App = {
         rotateSpeed: 1.0,
         panSpeed: 0.3,
         cameraNear: 10,
-        cameraFar: 5E5,
-        defaultCameraPosition: {x: -1000, y: 800, z: 1100}
+        cameraFar: 10000,
+        defaultCameraPosition: {x: 0, y: 50, z: 200}
     }
 };
 
 define(["dmu/SceneManager","dmu/LoaderManager"],
 function(SceneManager,LoaderManager){
+
     function PermalinkApp(fileName, width, height){
         var loaderManager = new LoaderManager({progressBar: true});
-        App.sceneManager = new SceneManager();
-        App.sceneManager.initRenderer = function() {
-            this.renderer = new THREE.WebGLRenderer({alpha: true});
-            this.renderer.setSize(width, height);
-            this.$container.append(this.renderer.domElement);
-        };
-        App.sceneManager.init();
+        var container = $("#container");
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(45, container.width() / container.height(), App.SceneOptions.cameraNear, App.SceneOptions.cameraFar);
+        camera.position.copy(App.SceneOptions.defaultCameraPosition);
+        addLightsToCamera(camera);
+        var renderer = new THREE.WebGLRenderer({alpha: true});
+        renderer.setSize(width, height);
+        container.append(renderer.domElement);
+        scene.add(camera);
+        scene.updateMatrixWorld();
+
+        var control = new THREE.TrackballControls(camera, container[0]);
         var texturePath = fileName.substring(0, fileName.lastIndexOf('/'));
         loaderManager.parseFile(fileName,texturePath,{
             success: function(geometry, material){
                 THREE.GeometryUtils.center(geometry);
-                App.sceneManager.scene.add(new THREE.Mesh(geometry, material));
-                //var boundingBox = mesh.geometry.boundingBox;
-                //var cog = new THREE.Vector3((boundingBox.max.x-boundingBox.min.x)/2,(boundingBox.max.y-boundingBox.min.y)/2,(boundingBox.max.z-boundingBox.min.z)/2);
-                //var radius = Math.max(boundingBox.size().x,boundingBox.size().y,boundingBox.size().z);
-                //sceneManager.placeCamera(cog,radius);                                                                 //Todo place camera on the mesh
+                var mesh = new THREE.Mesh(geometry, material);
+                scene.add(mesh);
+                centerOn(mesh);
             }
         });
+
+        control.addEventListener('change',function(){
+            render();
+        });
+
+        animate();
+
+        function centerOn (mesh) {
+            var boundingBox = mesh.geometry.boundingBox;
+            var cog = new THREE.Vector3().copy(boundingBox.center()).applyMatrix4(mesh.matrix);
+            var size = boundingBox.size();
+            var radius = Math.max(size.x, size.y, size.z);
+            var dir = new THREE.Vector3().copy(cog).sub(camera.position).normalize();
+            var distance = radius ? radius * 2 : 1000;
+            distance = distance < App.SceneOptions.cameraNear ? App.SceneOptions.cameraNear + 100 : distance;
+            var endCamPos = new THREE.Vector3().copy(cog).sub(dir.multiplyScalar(distance));
+            camera.position.copy(endCamPos);
+        }
+
+        function render() {
+            scene.updateMatrixWorld();
+            renderer.render(scene, camera);
+        }
+
+        function animate() {
+            requestAnimationFrame(animate, null);
+            control.update();
+            render();
+        }
+
+        function addLightsToCamera(camera) {
+            var dirLight = new THREE.DirectionalLight(0xffffff);
+            dirLight.position.set(200, 200, 1000).normalize();
+            camera.add(dirLight);
+            camera.add(dirLight.target);
+        }
+
     }
     return PermalinkApp;
 });
