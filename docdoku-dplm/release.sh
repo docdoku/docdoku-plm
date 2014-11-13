ba3895fb1c6bcf04e7accb22305f299a1428eb24
morgan <morgan.guimard@docdoku.com>
2014-11-13 16:50:26 +0100

    moving dplm

---
+++ docdoku-dplm/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# DPLM Gui Releaser
+#
+# Author : Morgan Guimard
+# Date : Wed Oct 2 2013
+#
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#bash build-cli.sh;
+#bash download.sh;
+#bash zip-ui.sh;
+#bash linux-all-platforms.sh;
+#bash windows-all-platforms.sh;
+#bash osx-all-platforms.sh;
+bash export.sh;
