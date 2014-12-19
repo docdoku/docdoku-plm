2a1e470e233d9007e8a22d3e7ab10c175aeb2f5e
morgan <morgan.guimard@docdoku.com>
2014-12-19 16:00:34 +0100

    tests added

--- docdoku-server/docdoku-server-web/yo/tests/config.ci.js
+++ docdoku-server/docdoku-server-web/yo/tests/config.ci.js
@@ -22,32 +22,32 @@ module.exports = {
     paths: [
         "js/auth/login.js",
         "js/pre/clean.js",
-        //
-        //"js/product-management/part/partCreation.js",
-        //"js/product-management/part/partCheckin.js",
-        //"js/product-management/part/showPartDetails.js",
-        //"js/product-management/product/productCreation.js",
-        //
-        //"js/product-management/baseline/baselineCreation.js",
-        //"js/product-management/baseline/baselineDuplication.js",
-        //
-        //"js/document-management/folder/folderCreation.js",
-        //"js/document-management/document/documentCreation.js",
-        //"js/document-management/document/documentDeletion.js",
-        //"js/document-management/template/templateCreation.js",
-        //
-        //"js/change-management/issue/issueCreation.js",
-        //"js/change-management/request/requestCreation.js",
-        //"js/change-management/order/orderCreation.js",
-        //"js/change-management/milestone/milestoneCreation.js",
+
+        "js/product-management/part/partCreation.js",
+        "js/product-management/part/partCheckin.js",
+        "js/product-management/part/showPartDetails.js",
+        "js/product-management/product/productCreation.js",
+
+        "js/product-management/baseline/baselineCreation.js",
+        "js/product-management/baseline/baselineDuplication.js",
+
+        "js/document-management/folder/folderCreation.js",
+        "js/document-management/document/documentCreation.js",
+        "js/document-management/document/documentDeletion.js",
+        "js/document-management/template/templateCreation.js",
+
+        "js/change-management/issue/issueCreation.js",
+        "js/change-management/request/requestCreation.js",
+        "js/change-management/order/orderCreation.js",
+        "js/change-management/milestone/milestoneCreation.js",
         "js/change-management/role/roleCreation.js",
-        //
-        //"js/document-management/template/templateDeletion.js",
-        //"js/document-management/folder/folderDeletion.js",
-        //
-        //"js/product-management/baseline/baselineDeletion.js",
-        //"js/product-management/product/productDeletion.js",
-        //"js/product-management/part/partDeletion.js"
+
+        "js/document-management/template/templateDeletion.js",
+        "js/document-management/folder/folderDeletion.js",
+
+        "js/product-management/baseline/baselineDeletion.js",
+        "js/product-management/product/productDeletion.js",
+        "js/product-management/part/partDeletion.js"
 
     ]
 };
