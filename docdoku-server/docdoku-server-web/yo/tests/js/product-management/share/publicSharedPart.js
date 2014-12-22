bfbce48f393d34448bb6f5e3e2ce48273b4fd086
morgan <morgan.guimard@docdoku.com>
2014-12-22 15:49:46 +0100

    adding tests

---
+++ docdoku-server/docdoku-server-web/yo/tests/js/product-management/share/publicSharedPart.js
@@ -0,0 +1,46 @@
+/*global casper,urls,workspace,documents*/
+
+casper.test.begin('Public shared document tests suite',2, function publicSharedDocumentTestsSuite(){
+
+    'use strict';
+
+    casper.open('');
+
+    /**
+     * Open document management URL
+     * */
+
+    casper.then(function(){
+        this.open(urls.documentPermalink);
+    });
+
+    /**
+     * Check for document title
+     */
+
+    casper.then(function checkDocumentTitle(){
+        this.waitForSelector('#page > h3',function titleDisplayed(){
+            this.test.assertSelectorHasText('#page > h3',documents.document1.number+'-A');
+        },function fail() {
+            this.capture('screenshot/publicSharedDocument/checkDocumentTitle-error.png');
+            this.test.assert(false,'Title can not be found');
+        });
+    });
+
+    /**
+     * Check for document iteration note
+     */
+    casper.then(function checkIterationNote(){
+        this.click('.nav-tabs a[href="#tab-document-iteration"]');
+        this.waitForSelector('#page > h3',function iterationNoteDisplayed(){
+            this.test.assertSelectorHasText('#tab-document-iteration > table > tbody > tr:nth-child(4) > td',documents.document1.iterationNote);
+        },function fail() {
+            this.capture('screenshot/publicSharedDocument/checkIterationNote-error.png');
+            this.test.assert(false,'Iteration note can not be found');
+        });
+    });
+
+    casper.run(function allDone() {
+        this.test.done();
+    });
+});
