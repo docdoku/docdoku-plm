07b47b4125b629874d1950f5f96c98f3fee10510
Taylor LABEJOF <taylor.labejof@docdoku.com>
2015-01-16 14:51:13 +0100

    [Translate] Remove whitespace before "?" in english translate

--- docdoku-server/docdoku-server-web/yo/app/js/localization/nls/change-management.js
+++ docdoku-server/docdoku-server-web/yo/app/js/localization/nls/change-management.js
@@ -2,10 +2,10 @@
 define({
     root: {
         ADVICE_CREATE_WORKFLOW:"Here you can create processes that define the life cycle of your documents and your parts",
-        CONFIRM_DELETE_ISSUE:"Do you want to delete selected issues ?",
-        CONFIRM_DELETE_ORDER:"Do you want to delete selected orders ?",
-        CONFIRM_DELETE_REQUEST:"Do you want to delete selected requests ?",
-        CONFIRM_DELETE_WORKFLOW:"Do you want to delete selected workflows ?",
+        CONFIRM_DELETE_ISSUE:"Do you want to delete selected issues?",
+        CONFIRM_DELETE_ORDER:"Do you want to delete selected orders?",
+        CONFIRM_DELETE_REQUEST:"Do you want to delete selected requests?",
+        CONFIRM_DELETE_WORKFLOW:"Do you want to delete selected workflows?",
         ERROR_WORKFLOW_REFERENCE_MISSING:"You're trying to create a workflow without reference",
         NEW_ROLES_NAME:"Enter the new role name",
         WARNING_ANY_ROLE:"Create some role before create a Workflow",
