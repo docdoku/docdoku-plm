dd484549b8decd98487d122fc2a364dc8c86a001
morgan <morgan.guimard@docdoku.com>
2015-01-22 11:09:52 +0100

    Build script dor decimater, comments

--- docdoku-openmesh-decimater/open-mesh-installer.sh
+++ docdoku-openmesh-decimater/open-mesh-installer.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-# requirements cmake, g++, gcc, qt4/qt5
-# sudo apt-get install cmake g++ gcc
+# Requirements : OpenMesh
 # Please, visit http://www.openmesh.org/media/Documentations/OpenMesh-Doc-Latest/a00030.html for more informations.
 # to install qt on your server :
 
+apt-get install cmake g++ qt-sdk;
 mkdir -p openmesh;
 cd openmesh;
-[[ -f OpenMesh-3.3.tar.gz ]] || wget http://www.openmesh.org/media/Releases/3.3/OpenMesh-3.3.tar.gz
-tar -xzvf OpenMesh-3.3.tar.gz;
-cd OpenMesh-3.3;
+[[ -f OpenMesh-3.2.tar.gz ]] || wget http://www.openmesh.org/media/Releases/3.2/OpenMesh-3.2.tar.gz
+tar -xzvf OpenMesh-3.2.tar.gz;
+cd OpenMesh-3.2;
 mkdir build && cd $_;
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make install;
+cmake -DCMAKE_BUILD_TYPE=Release .. && make install;
