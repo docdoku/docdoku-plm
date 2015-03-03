6e7e0aec85734dcadf762d7db188cd81ca2a931b
Morgan <morgan.guimard@gmail.com>
2015-03-03 22:23:19 +0100

    SSh enabled on CLI

--- docdoku-cli/src/main/java/com/docdoku/cli/tools/ScriptingTools.java
+++ docdoku-cli/src/main/java/com/docdoku/cli/tools/ScriptingTools.java
@@ -35,31 +35,15 @@ import java.util.Map;
  */
 public class ScriptingTools {
 
-    public static final String HTTP_CLIENT_STREAMING_CHUNK_SIZE;
-    public static final String JAVA7_HTTP_CLIENT_STREAMING_CHUNK_SIZE = "com.sun.xml.internal.ws.transport.http.client.streaming.chunk.size";
-    public static final String JAVA6_HTTP_CLIENT_STREAMING_CHUNK_SIZE = "com.sun.xml.ws.transport.http.client.streaming.chunk.size";
-
-
     private static final String DOCUMENT_WSDL_LOCATION = "/services/document?wsdl";
     private static final String PRODUCT_WSDL_LOCATION = "/services/product?wsdl";
     private static final String PRODUCT_CONFIGSPEC_WSDL_LOCATION = "/services/productConfigSpec?wsdl";
-    private static final String WORKFLOW_WSDL_LOCATION = "/services/workflow?wsdl";
     private static final String USER_WSDL_LOCATION = "/services/user?wsdl";
-    private static final String FILE_MANAGER_WSDL_LOCATION = "/services/UploadDownload?wsdl";
 
     private static final String NAMESPACEURI = "http://server.docdoku.com/";
     private static final String PRODUCT_NAMESPACEURI = "http://products.server.docdoku.com/";
 
-    static {
-        String version = System.getProperty("java.version"); 
-        if (version.startsWith("1.7")) {
-            HTTP_CLIENT_STREAMING_CHUNK_SIZE = JAVA7_HTTP_CLIENT_STREAMING_CHUNK_SIZE;
-        } else {
-            HTTP_CLIENT_STREAMING_CHUNK_SIZE = JAVA6_HTTP_CLIENT_STREAMING_CHUNK_SIZE;
-        }
-    }
     private ScriptingTools() {
-        
     }
 
     public static IDocumentManagerWS createDocumentService(URL url, String login, String password) throws Exception {
@@ -85,30 +69,8 @@ public class ScriptingTools {
         IProductConfigSpecManagerWS port = service.getPort(IProductConfigSpecManagerWS.class);
         ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, login);
         ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
-
         return port;
     }
-
-    public static IWorkflowManagerWS createWorkflowService(URL url, String login, String password) throws Exception {
-        WorkflowService service = new WorkflowService(new URL(url, WORKFLOW_WSDL_LOCATION), new javax.xml.namespace.QName(NAMESPACEURI, "WorkflowManagerBeanService"));
-        IWorkflowManagerWS port = service.getPort(IWorkflowManagerWS.class);
-        ((BindingProvider) port).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, login);
-        ((BindingProvider) port).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
-
-        return port;
-    }
-
-    public static IUploadDownloadWS createFileManagerService(URL url, String login, String password) throws MalformedURLException {
-        MTOMFeature feature = new MTOMFeature();
-        UploadDownloadService service = new UploadDownloadService(new URL(url, FILE_MANAGER_WSDL_LOCATION), new javax.xml.namespace.QName(NAMESPACEURI, "UploadDownloadService"));
-        IUploadDownloadWS proxy = service.getPort(IUploadDownloadWS.class, feature);
-        Map context = ((BindingProvider) proxy).getRequestContext();
-        context.put(HTTP_CLIENT_STREAMING_CHUNK_SIZE, 8192);
-        context.put(BindingProvider.USERNAME_PROPERTY, login);
-        context.put(BindingProvider.PASSWORD_PROPERTY, password);
-        return proxy;
-    }
-
 
     public static IUserManagerWS createUserManagerService(URL url, String login, String password) throws MalformedURLException {
         UserService service = new UserService(new URL(url, USER_WSDL_LOCATION), new javax.xml.namespace.QName(NAMESPACEURI, "UserManagerBeanService"));
