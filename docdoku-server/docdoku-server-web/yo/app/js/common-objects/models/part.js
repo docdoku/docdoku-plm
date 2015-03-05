5a3386f2c1c1ead3fb6082aab6058dbc71849d56
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-03-05 11:44:40 +0100

    splitted isAssembly and isSheet info

--- docdoku-server/docdoku-server-web/yo/app/js/common-objects/models/part.js
+++ docdoku-server/docdoku-server-web/yo/app/js/common-objects/models/part.js
@@ -148,9 +148,9 @@ function (Backbone, Date, PartIterationList, ACLChecker,AlertView) {
             return false;
         },
 
-        isLastIteration3DSheet: function () {
+        hasLastIterationAttachedFiles: function () {
             if (this.hasIterations()) {
-                return this.getLastIteration().getNativeCADFile();
+                return this.getLastIteration().getAttachedFiles();
             }
             return false;
         },
