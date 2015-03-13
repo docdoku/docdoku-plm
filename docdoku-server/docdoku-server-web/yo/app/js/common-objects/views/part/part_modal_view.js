b8ffba82cb6a6e2409ceb983b3985c7e13bda384
morgan <morgan.guimard@docdoku.com>
2015-03-13 16:51:48 +0100

    Null pointer in part resource => the request set from the client wasn't on a correct object, need to fetch the model after last iteration is saved

--- docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/part/part_modal_view.js
+++ docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/part/part_modal_view.js
@@ -173,6 +173,7 @@ define([
                     if (that.model.collection){
                         that.model.collection.fetch();
                     }
+                    that.model.fetch();
                     that.hide();
                     that.model.trigger('change');
                 },
