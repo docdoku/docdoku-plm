197e607d94b7ba6fa2c5b416ba9945e7ad13fe57
morgan <morgan.guimard@docdoku.com>
2015-03-13 14:04:35 +0100

    closes #446

--- docdoku-server/docdoku-server-web/yo/app/js/localization/nls/fr/change-management.js
+++ docdoku-server/docdoku-server-web/yo/app/js/localization/nls/fr/change-management.js
@@ -5,9 +5,9 @@ define({
     CONFIRM_DELETE_ORDER:"Voulez-vous supprimer les ordres sélectionnés",
     CONFIRM_DELETE_REQUEST:"Voulez-vous supprimer les demandes sélectionnées",
     CONFIRM_DELETE_WORKFLOW:"Voulez-vous supprimer les processus sélectionnées",
-    ERROR_WORKFLOW_REFERENCE_MISSING:"Veuillez renseigner le nom de votre workflow",
+    ERROR_WORKFLOW_REFERENCE_MISSING:"Veuillez renseigner le nom de votre processus",
     NEW_ROLES_NAME:"Entrer le nom du nouveau rôle",
-    WARNING_ANY_ROLE:"Veuillez créer des rôles avant de créer un workflow",
-    WARNING_FINAL_STATE_MISSING:"Vous devriez renseigner le nom de l'état final de votre workflow",
+    WARNING_ANY_ROLE:"Veuillez créer des rôles avant de créer un processus",
+    WARNING_FINAL_STATE_MISSING:"Vous devriez renseigner le nom de l'état final de votre processus",
     WARNING_ACTIVITIES_MISSING:"Vous devriez créer au moins une activité et des tâches"
 });
