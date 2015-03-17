23458ccb04300559cca0bb0a7694aeeb8e7c90fb
Lebeau Julien <julien.lebeau@docdoku.com>
2015-03-17 17:30:10 +0100

    fix render attribut twice when click on add

--- docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/attributes/attributes.js
+++ docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/attributes/attributes.js
@@ -28,7 +28,6 @@ define([
 
         setAttributesLocked: function (attributesLocked) {
             this.attributesLocked = attributesLocked;
-            this.render();
         },
 
         initialize: function () {
