c6f33b25307ec19e6226e128c41d21001d4172a0
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-03-23 16:17:03 +0100

    added description text on top of list

--- docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/part/modification_notification_list_view.js
+++ docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/part/modification_notification_list_view.js
@@ -1,13 +1,16 @@
 /*global _,$,define,App*/
 define([
     'backbone',
+    'mustache',
     'common-objects/views/part/modification_notification_list_item_view',
-    'common-objects/models/modification_notification'
-], function (Backbone, ModificationNotificationListItemView, ModificationNotification) {
+    'common-objects/models/modification_notification',
+    'text!common-objects/templates/part/modification_notification_list.html'
+], function (Backbone, Mustache, ModificationNotificationListItemView, ModificationNotification, template) {
     'use strict';
     var ModificationNotificationListView = Backbone.View.extend({
 
         render: function () {
+            this.$el.html(Mustache.render(template, {i18n: App.config.i18n}));
 
             this.modificationNotificationViews = [];
 
