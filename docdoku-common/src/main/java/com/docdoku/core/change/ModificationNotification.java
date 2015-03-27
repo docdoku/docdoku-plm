53d75b07a03bd2d796539cb51b68a3dd58b7d293
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-03-27 12:01:27 +0100

    enhanced modification notification interface

--- docdoku-common/src/main/java/com/docdoku/core/change/ModificationNotification.java
+++ docdoku-common/src/main/java/com/docdoku/core/change/ModificationNotification.java
@@ -39,7 +39,7 @@ import java.util.Date;
  */
 @Entity
 @NamedQueries ({
-        @NamedQuery(name="ModificationNotification.findByImpactedPartIteration", query = "SELECT n FROM ModificationNotification n WHERE n.impactedPart.iteration = :iteration AND n.impactedPart.partRevision.version = :version AND n.impactedPart.partRevision.partMaster.number = :partNumber AND n.impactedPart.partRevision.partMaster.workspace.id = :workspaceId ORDER BY n.modifiedPart.modificationDate"),
+        @NamedQuery(name="ModificationNotification.findByImpactedPartIteration", query = "SELECT n FROM ModificationNotification n WHERE n.impactedPart.iteration = :iteration AND n.impactedPart.partRevision.version = :version AND n.impactedPart.partRevision.partMaster.number = :partNumber AND n.impactedPart.partRevision.partMaster.workspace.id = :workspaceId ORDER BY n.acknowledged, n.modifiedPart.modificationDate"),
         @NamedQuery(name="ModificationNotification.removeAllOnPartRevision", query = "DELETE FROM ModificationNotification n WHERE n.impactedPart.partRevision.version = :version AND n.impactedPart.partRevision.partMaster.number = :partNumber AND n.impactedPart.partRevision.partMaster.workspace.id = :workspaceId")
 })
 @Table(name="MODIFICATIONNOTIFICATION")
