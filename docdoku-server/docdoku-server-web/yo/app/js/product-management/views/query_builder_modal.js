213f02c072b7ca9d88a125da0d534d2e7d1d71f1
Lebeau Julien <julien.lebeau@docdoku.com>
2015-04-10 10:51:17 +0200

    retrieve information from select and query builder

--- docdoku-server/docdoku-server-web/yo/app/js/product-management/views/query_builder_modal.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-management/views/query_builder_modal.js
@@ -5,20 +5,25 @@ define([
     'text!templates/query_builder_modal.html',
     'query-builder',
     'selectize',
-    '../../utils/query-builder-options'
-], function (Backbone, Mustache, template, queryBuilder, selectize,querybuilderOptions) {
+    '../../utils/query-builder-options',
+    'common-objects/views/alert'
+], function (Backbone, Mustache, template, queryBuilder, selectize,querybuilderOptions, AlertView) {
     'use strict';
     var QueryBuilderModal = Backbone.View.extend({
 
         events: {
-            'hidden #query-builder-modal': 'onHidden'
+            'hidden #query-builder-modal': 'onHidden',
+            'click .search-button': 'onSearch'
         },
+
+        delimiter: '/',
 
         initialize: function () {
             this.selectizeOptions = {
                 plugins: ['remove_button','drag_drop'],
                 persist: false,
-                valueField: 'name',
+                delimiter:this.delimiter,
+                valueField: 'value',
                 searchField: ['name'],
                 options: querybuilderOptions.fields,
                 render: {
@@ -37,7 +42,7 @@ define([
             this.bindDomElements();
             this.fillSelectizes();
 
-            this.$('#builder').queryBuilder({
+            this.$where.queryBuilder({
                 plugins: [
                     'bt-tooltip-errors'
                 ],
@@ -52,6 +57,7 @@ define([
                 }
 
             });
+
             return this;
         },
 
@@ -63,9 +69,46 @@ define([
 
         bindDomElements: function () {
             this.$modal = this.$('#query-builder-modal');
+            this.$where = this.$('#where');
             this.$select = this.$('#select');
             this.$orderBy = this.$('#orderBy');
             this.$groupBy = this.$('#groupBy');
+        },
+
+        onSearch:function(){
+
+            var selectList = this.$select[0].selectize.getValue().split(this.delimiter);
+            var where = this.$where.queryBuilder('getRules');
+            var orderByList = this.$orderBy[0].selectize.getValue().split(this.delimiter);
+            var groupByList = this.$groupBy[0].selectize.getValue().split(this.delimiter);
+
+            var saveQuery = false;
+
+            var data = {
+                selects : selectList,
+                orderByList : orderByList,
+                groupedByList : groupByList,
+                rules : where
+            }
+
+            var url = App.config.contextPath + '/api/workspaces/' + App.config.workspaceId + '/parts/queries?save='+saveQuery;
+            $.ajax({
+                type: 'POST',
+                url : url,
+                data: JSON.stringify(data),
+                contentType:'application/json',
+                success: function(data){
+                    debugger
+                },
+                error : function(errorMessage){
+                    self.$('#alerts').append(new AlertView({
+                        type: 'error',
+                        message: errorMessage
+                    }).render().$el);
+                }
+            });
+
+
         },
 
         openModal: function () {
