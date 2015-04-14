f5bb306c24517cfa566edcef50d9aa0d94280d2e
Lebeau Julien <julien.lebeau@docdoku.com>
2015-04-14 12:20:30 +0200

    fix space in group

--- docdoku-server/docdoku-server-web/yo/app/js/product-management/views/part/part_grouped_by_list.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-management/views/part/part_grouped_by_list.js
@@ -14,6 +14,19 @@ define([
 
         initialize: function () {
             this.items = this.options.data.queryResponse;
+
+            this.items = [
+                {"pm.number":"ARM-0001","pm.name":"Armoire TOP","pm.type":"Armoires simu"},
+                {"pm.number":"ARM-0001","pm.name":"Armoire TOP","pm.type":"Armoires simu"},
+                {"pm.number":"ARM-0001","pm.name":"Armoire TOP","pm.type":"Armoires simu"},
+                {"pm.number":"ARM-0002","pm.name":"Armoire TIP","pm.type":"Armoires simu"},
+                {"pm.number":"ARM-0002","pm.name":"Armoire TIP","pm.type":"Armoires simu"},
+                {"pm.number":"ARM-0002","pm.name":"Armoire TIP","pm.type":"Armoires"},
+                {"pm.number":"ARM-0003","pm.name":"ARMTAP","pm.type":"Armoires"},
+                {"pm.number":"ARM-0003","pm.name":"ARMTAP","pm.type":"Armoires"},
+                {"pm.number":"ARM-0003","pm.name":"ARMTAP","pm.type":"Armoires"}
+            ];
+
             this.selects = this.options.data.queryData.selects;
             this.orderByList = this.options.data.queryData.orderByList;
             this.groupedByList = this.options.data.queryData.groupedByList;
@@ -23,15 +36,22 @@ define([
 
             var itemsGroupBy = this.groupBy();
 
+            var groups = {};
+            var i = 0;
+            _.each(_.keys(itemsGroupBy), function(key){
+                groups[''+i] = itemsGroupBy[key];
+                i++;
+            });
+
             this.$el.html(Mustache.render(template, {
                 i18n: App.config.i18n,
                 columns:this.selects,
-                groups: _.keys(itemsGroupBy)
+                groups:_.keys(groups)
             }));
 
             var self = this;
-            _.each(_.keys(itemsGroupBy),function(key){
-                var values = self.orderBy(itemsGroupBy[key]);
+            _.each(_.keys(groups),function(key){
+                var values = self.orderBy(groups[key]);
                 _.each(values, function(item){
                     var itemView = new PartGroupedByListItemView({
                         item : item
@@ -47,6 +67,7 @@ define([
             var self = this;
 
             return _.groupBy(this.items,function(item){
+
                 var groupByStringToUse = "";
                 _.each(self.groupedByList, function(groupByColumn){
                     groupByStringToUse = groupByStringToUse+'_'+item[groupByColumn];
@@ -59,12 +80,18 @@ define([
 
         orderBy:function(items){
             var self = this;
-            return _.orderBy(items, function(item){
-                var orderByStringToUse = "";
+            return items.sort(function(item1, item2){
+
+                var item1String = "";
+                var item2String = "";
                 _.each(self.orderByList, function(orderByColumn){
-                    orderByStringToUse = orderByStringToUse+'_'+item[orderByColumn];
+                    item1String = item1String+'_'+item1[orderByColumn];
+                    item2String = item2String+'_'+item2[orderByColumn];
                 });
-                return orderByStringToUse.substring(1);
+                item1String = item1String.substring(1);
+                item2String = item2String.substring(1);
+
+                return item1String < item2String;
             });
         }
     });
