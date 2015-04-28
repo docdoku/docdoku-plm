a79768420700be6f2bfa6c7ba426e187c2c8da5d
morgan <morgan.guimard@docdoku.com>
2015-04-28 14:27:24 +0200

    DPLM build : exit on any error. Closes #482

--- docdoku-dplm/build-dplm.sh
+++ docdoku-dplm/build-dplm.sh
@@ -6,4 +6,5 @@ SOURCES=${BASE_DIR}/ui
 echo "Building docdoku-dplm ... ";
 cd ${SOURCES}
 grunt build;
+[[ $? -eq 0 ]] || { echo "Grunt build failed"; exit 1; }
 echo "... done"
--- docdoku-dplm/release.sh
+++ docdoku-dplm/release.sh
@@ -12,10 +12,19 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p tmp;
 
 bash build-cli.sh;
+[[ $? -eq 0 ]] || { echo "Build cli failed"; exit $?; }
 bash build-dplm.sh;
+[[ $? -eq 0 ]] || { echo "Build dplm failed"; exit $?; }
 bash download.sh;
+[[ $? -eq 0 ]] || { echo "Download nw failed"; exit $?; }
 bash zip-ui.sh;
+[[ $? -eq 0 ]] || { echo "Zipping app failed"; exit $?; }
 bash linux-all-platforms.sh;
+[[ $? -eq 0 ]] || { echo "Linux build failed"; exit $?; }
 bash windows-all-platforms.sh;
+[[ $? -eq 0 ]] || { echo "Windows build failed"; exit $?; }
 bash osx-all-platforms.sh;
+[[ $? -eq 0 ]] || { echo "OSX build failed"; exit $?; }
 bash export.sh;
+[[ $? -eq 0 ]] || { echo "Export built app failed"; exit $?; }
+
