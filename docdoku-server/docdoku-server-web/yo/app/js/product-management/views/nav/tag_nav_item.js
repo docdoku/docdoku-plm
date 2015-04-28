a4da778565f5b31cf4e69b3129c38f888b700ec5
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-04-28 18:04:43 +0200

    fixed #517

--- docdoku-server/docdoku-server-web/yo/app/js/product-management/views/nav/tag_nav_item.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-management/views/nav/tag_nav_item.js
@@ -2,16 +2,26 @@
 define([
     'backbone',
     'mustache',
-    'text!templates/nav/tag_nav_item.html'
-], function (Backbone, Mustache, template) {
+    'text!templates/nav/tag_nav_item.html',
+    'common-objects/models/part'
+], function (Backbone, Mustache, template, Part) {
 	'use strict';
     var TagNavItemView = Backbone.View.extend({
         events:{
             'click .delete': 'actionDelete',
-            'mouseleave .header': 'hideActions'
+            'mouseleave .header': 'hideActions',
+            'dragenter >.nav-list-entry': 'onDragEnter',
+            'dragover >.nav-list-entry': 'checkDrag',
+            'dragleave >.nav-list-entry': 'onDragLeave',
+            'drop >.nav-list-entry': 'onDrop'
         },
         render: function () {
-            this.$el.html(Mustache.render(template, {i18n: App.config.i18n, workspaceId: App.config.workspaceId, model:this.model}));
+            this.$el.html(Mustache.render(template, {
+                i18n: App.config.i18n,
+                workspaceId: App.config.workspaceId,
+                model:this.model
+            }));
+            this.tagDiv = this.$('>.nav-list-entry');
             return this;
         },
         hideActions:function() {
@@ -28,7 +38,42 @@ define([
                 }
             });
             return false;
+        },
+
+        onDragEnter: function () {
+            this.tagDiv.hasClass('move-part-into');
+        },
+
+        checkDrag: function (e) {
+            e.dataTransfer.dropEffect = 'copy';
+            this.tagDiv.addClass('move-part-into');
+            return false;
+        },
+
+        onDragLeave: function (e) {
+            e.dataTransfer.dropEffect = 'none';
+            this.tagDiv.removeClass('move-part-into');
+        },
+
+        onDrop: function (e) {
+            if (e.dataTransfer.getData('part:text/plain')) {
+                this.tagPart(e);
+            }
+        },
+
+        tagPart: function(e) {
+            var that = this;
+            var part = new Part(JSON.parse(e.dataTransfer.getData('part:text/plain')));
+
+            part.addTags([this.model]).success(function () {
+                that.tagDiv.removeClass('move-part-into');
+                that.tagDiv.highlightEffect();
+            }).error(function () {
+                that.tagDiv.removeClass('move-part-into');
+            });
+
         }
+
     });
 
     return TagNavItemView;
--- docdoku-server/docdoku-server-web/yo/app/js/product-management/views/part/part_list_item.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-management/views/part/part_list_item.js
@@ -16,7 +16,9 @@ define([
             'click td.modification_notification i': 'toPartModalOnNotificationsTab',
             'click td.part_number': 'toPartModal',
             'click td.part-revision-share i': 'sharePart',
-            'click td.part-attached-files i': 'toPartModalOnFilesTab'
+            'click td.part-attached-files i': 'toPartModalOnFilesTab',
+            'dragstart a.parthandle': 'dragStart',
+            'dragend a.parthandle': 'dragEnd'
         },
 
         tagName: 'tr',
@@ -27,6 +29,10 @@ define([
 
             this.listenTo(this.model, 'change', this.render);
             this.listenTo(this.model, 'sync', this.render);
+
+            // jQuery creates it's own event object, and it doesn't have a
+            // dataTransfer property yet. This adds dataTransfer to the event object.
+            $.event.props.push('dataTransfer');
         },
 
         render: function () {
@@ -105,6 +111,35 @@ define([
             var shareView = new ShareView({model: this.model, entityType: 'parts'});
             window.document.body.appendChild(shareView.render().el);
             shareView.openModal();
+        },
+
+        dragStart: function (e) {
+            var that = this;
+            this.$el.addClass('moving');
+
+            Backbone.Events.on('part-moved', function () {
+                Backbone.Events.off('part-moved');
+                Backbone.Events.off('part-error-moved');
+                that.model.collection.remove(that.model);
+            });
+            Backbone.Events.on('part-error-moved', function () {
+                Backbone.Events.off('part-moved');
+                Backbone.Events.off('part-error-moved');
+                that.$el.removeClass('moving');
+            });
+            var data = JSON.stringify(this.model);
+            e.dataTransfer.setData('part:text/plain', data);
+            e.dataTransfer.dropEffect = 'none';
+            e.dataTransfer.effectAllowed = 'copyMove';
+            return e;
+        },
+
+        dragEnd: function (e) {
+            if (e.dataTransfer.dropEffect === 'none') {
+                Backbone.Events.off('part-moved');
+                Backbone.Events.off('part-error-moved');
+            }
+            this.$el.removeClass('moving');
         }
 
     });
