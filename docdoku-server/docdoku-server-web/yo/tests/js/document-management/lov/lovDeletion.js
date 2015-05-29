642cc5bb579f51f242b730b23cbabb6dfbe6d870
kelto <charles.fallourd@gmail.com>
2015-05-29 11:21:15 +0200

    add a wait for the modal to be closed in casper tests

--- docdoku-server/docdoku-server-web/yo/tests/js/document-management/lov/lovDeletion.js
+++ docdoku-server/docdoku-server-web/yo/tests/js/document-management/lov/lovDeletion.js
@@ -1,6 +1,6 @@
 /*global casper,urls,documents*/
 
-casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTestsSuite(){
+casper.test.begin('LOV deletion tests suite', 2, function documentLOVDeletionTestsSuite() {
 
     'use strict';
 
@@ -10,7 +10,7 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
      * Open document management URL
      * */
 
-    casper.then(function(){
+    casper.then(function () {
         this.open(urls.documentManagement);
     });
 
@@ -18,8 +18,8 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
      * Open template nav
      */
 
-    casper.then(function waitForTemplateNavLink(){
-        this.waitForSelector('#template-nav > .nav-list-entry > a',function clickTemplateNavLink() {
+    casper.then(function waitForTemplateNavLink() {
+        this.waitForSelector('#template-nav > .nav-list-entry > a', function clickTemplateNavLink() {
             this.click('#template-nav > .nav-list-entry > a');
         });
     });
@@ -28,8 +28,8 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
      * Open LOV creation modal
      */
 
-    casper.then(function waitForLOVCreationLink(){
-        this.waitForSelector('.actions .list-lov',function clickOnLOVCreationLink(){
+    casper.then(function waitForLOVCreationLink() {
+        this.waitForSelector('.actions .list-lov', function clickOnLOVCreationLink() {
             this.click('.actions .list-lov');
         });
     });
@@ -38,8 +38,8 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
      * Check the number of item
      */
 
-    casper.then(function waitForLOVCreationModal(){
-        this.waitForSelector('.list-lov',function checkLOVPersistence(){
+    casper.then(function waitForLOVCreationModal() {
+        this.waitForSelector('.list-lov', function checkLOVPersistence() {
             this.test.assertExists('.lovItem', "One element should be in the list of LOV");
         });
     });
@@ -47,16 +47,19 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
     /**
      * Delete the item and save
      */
-    casper.then(function deleteItem(){
+    casper.then(function deleteItem() {
         this.click('.deleteLovItem');
         this.click('.btn-saveLovs');
+        this.waitWhileSelector('.modal', function() {
+            this.echo('modal closed');
+        });
     });
 
     /**
      * Re-open the modal to check
      */
-    casper.then(function reopenModal(){
-        this.waitForSelector('.actions .list-lov',function clickOnLOVCreationLink(){
+    casper.then(function reopenModal() {
+        this.waitForSelector('.actions .list-lov', function clickOnLOVCreationLink() {
             this.click('.actions .list-lov');
         });
     });
@@ -65,20 +68,19 @@ casper.test.begin('LOV deletion tests suite',2, function documentLOVDeletionTest
      * Check the number of item
      */
 
-    casper.then(function waitForLOVCreationModal(){
-        this.waitForSelector('.list-lov',function checkDeletion(){
-            this.waitWhileSelector('.lovItem',function templateDeleted(){
-                this.test.assert(true,'LOV deleted');
-            },function fail(){
-                this.capture('screenshot/lov/lovDeletion-error.png');
-                this.test.assert(false,'love still there');
-            });
+    casper.then(function waitForLOVCreationModal() {
+         this.waitForSelector('.list-lov', function checkDeletion() {
+            try {
+                this.test.assertDoesntExist('.lovItem', 'LOV deleted');
+            } catch (e) {
+                this.test.assert(false, 'love still there');
+            }
         });
     });
 
-    casper.run(function allDone() {
-        this.test.done();
-    });
+casper.run(function allDone() {
+    this.test.done();
+});
 });
 /**
  * Created by lebeaujulien on 12/03/15.
