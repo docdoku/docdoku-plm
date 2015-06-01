efc7ba4ed681afd7e07f14accbf45df2153a9b7a
kelto <charles.fallourd@gmail.com>
2015-06-01 14:44:17 +0200

    fix date handling, now the server send the datetime in UTC

---
+++ docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/util/DateAdapter.java
@@ -0,0 +1,31 @@
+package com.docdoku.server.rest.util;
+
+/**
+ * Created by kelto on 01/06/15.
+ */
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+
+public class DateAdapter extends XmlAdapter<String, Date> {
+
+    // the desired format
+    private static String pattern = "yyyy-MM-dd'T'hh:mm:ss";
+
+    public String marshal(Date date) throws Exception {
+        if(date == null) {
+            return "";
+        }
+        SimpleDateFormat df =  new SimpleDateFormat(pattern);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return df.format(date);
+    }
+
+    public Date unmarshal(String dateString) throws Exception {
+        //TODO: check if we have to specify UTC
+        return new SimpleDateFormat(pattern).parse(dateString);
+    }
+}
