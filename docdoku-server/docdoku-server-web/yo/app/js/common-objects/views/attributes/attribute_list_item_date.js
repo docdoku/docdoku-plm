fdf35d1f4661415d913ffff55a79a06f4da88c6f
kelto <charles.fallourd@gmail.com>
2015-06-02 16:27:30 +0200

    fix local visualization of attributes date when they are locked

--- docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/attributes/attribute_list_item_date.js
+++ docdoku-server/docdoku-server-web/yo/app/js/common-objects/views/attributes/attribute_list_item_date.js
@@ -25,10 +25,12 @@ define([
          * format date from attribute model (timestamp string) to html5 input date ('yyyy-mm-dd')
          */
         modelToJSON: function () {
+            var format = this.editMode && !this.model.get('locked') ? App.config.i18n._DATE_PICKER_DATE_FORMAT
+                : App.config.i18n._DATE_SHORT_FORMAT;
             var data = this.model.toJSON();
             if (!_.isEmpty(data.value)) {
                 data.value = date.formatLocalTime(
-                    App.config.i18n._DATE_PICKER_DATE_FORMAT,
+                    format,
                     new Date(data.value)
                 );
             }
