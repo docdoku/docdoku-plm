8427923f5370d4612fa397e3ceb4bb9679c9162e
morgan <morgan.guimard@docdoku.com>
2015-06-15 15:51:26 +0200

    Typo, closes #660

--- docdoku-server/docdoku-server-web/yo/app/js/localization/nls/fr/product-management.js
+++ docdoku-server/docdoku-server-web/yo/app/js/localization/nls/fr/product-management.js
@@ -1,10 +1,10 @@
 /*global define*/
 define({
     CONFIRM_DELETE_BASELINE:"Voulez-vous supprimer les baselines sélectionnées",
-    CONFIRM_DELETE_PART:"Voulez-vous supprimer les articles sélectionnées",
-    CONFIRM_DELETE_PART_TEMPLATE:"Voulez-vous supprimer les modèles d'articles sélectionnées",
-    CONFIRM_DELETE_PRODUCT:"Voulez-vous supprimer les produits sélectionnées",
-    CONFIRM_DELETE_PRODUCT_INSTANCE:"Voulez-vous supprimer les exemplaires de produits sélectionnées",
+    CONFIRM_DELETE_PART:"Voulez-vous supprimer les articles sélectionnés",
+    CONFIRM_DELETE_PART_TEMPLATE:"Voulez-vous supprimer les modèles d'articles sélectionnés",
+    CONFIRM_DELETE_PRODUCT:"Voulez-vous supprimer les produits sélectionnés",
+    CONFIRM_DELETE_PRODUCT_INSTANCE:"Voulez-vous supprimer les exemplaires de produits sélectionnés",
     CONFIRM_DELETE_CONFIGURATION:"Voulez-vous supprimer les configurations sélectionnées",
     CREATE_BASELINE_LATEST: "Créer la baseline avec les derniers articles",
     CREATE_BASELINE_RELEASED: "Créer la baseline avec les derniers articles finalisés",
