4522c295b9d779b4698383b1a3fa912c46c4201d
kelto <charles.fallourd@gmail.com>
2015-06-26 10:56:52 +0200

    add package-info to dto/product to apply DateAdapter on all date to this package

---
+++ docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/dto/product/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by kelto on 01/06/15.
+ */
+@XmlJavaTypeAdapter(value=DateAdapter.class, type=Date.class)
+package com.docdoku.server.rest.dto.product;
+
+import com.docdoku.server.rest.util.DateAdapter;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Date;
