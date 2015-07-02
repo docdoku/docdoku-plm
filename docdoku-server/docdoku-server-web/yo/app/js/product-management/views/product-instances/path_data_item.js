630eabd51e622f5dac8bbeb7be19ae5eda31463a
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-07-02 12:54:01 +0200

    show chevron on each path (including with parts with name...)

--- docdoku-server/docdoku-server-web/yo/app/js/product-management/views/product-instances/path_data_item.js
+++ docdoku-server/docdoku-server-web/yo/app/js/product-management/views/product-instances/path_data_item.js
@@ -16,7 +16,8 @@ define([
 
             for (var i=0; i<this.model.length; i++) {
                 var part = this.model[i];
-                path += part.name ? part.name + ' < ' + part.number + ' >' : '< ' + part.number + ' > <i class="fa fa-chevron-right"></i> ';
+                path += part.name ? part.name + ' < ' + part.number + ' >' : '< ' + part.number + ' >';
+                path += ' <i class="fa fa-chevron-right"></i> ';
             }
 
             this.$el.html(template);
