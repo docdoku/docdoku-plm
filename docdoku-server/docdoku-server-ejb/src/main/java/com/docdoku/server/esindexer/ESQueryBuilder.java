a97064df36e69e45ac4ac45cdb383ee88ae9fc9d
morgan <morgan.guimard@docdoku.com>
2015-07-16 11:44:53 +0200

    Collection.isEmpty() should be used to test for emptiness

--- docdoku-server/docdoku-server-ejb/src/main/java/com/docdoku/server/esindexer/ESQueryBuilder.java
+++ docdoku-server/docdoku-server-ejb/src/main/java/com/docdoku/server/esindexer/ESQueryBuilder.java
@@ -1,3 +1,23 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2015 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.docdoku.server.esindexer;
 
 import org.elasticsearch.index.query.*;
@@ -14,16 +34,16 @@ public class ESQueryBuilder {
     private final List<QueryBuilder> queries;
 
     public ESQueryBuilder() {
-        this.filters = new ArrayList<>();
-        this.queries = new ArrayList<>();
+        filters = new ArrayList<>();
+        queries = new ArrayList<>();
     }
 
     public void add(QueryBuilder query) {
-        this.queries.add(query);
+        queries.add(query);
     }
 
     public void add(FilterBuilder filter) {
-        this.filters.add(filter);
+        filters.add(filter);
     }
 
     public QueryBuilder getFilteredQuery() {
@@ -32,9 +52,9 @@ public class ESQueryBuilder {
 
     private QueryBuilder getQuery() {
         QueryBuilder query;
-        if(this.queries.size() > 0) {
+        if(!queries.isEmpty()) {
             BoolQueryBuilder bqr = QueryBuilders.boolQuery();
-            for(QueryBuilder qr : this.queries) {
+            for(QueryBuilder qr : queries) {
                 bqr.must(qr);
             }
             query = bqr;
@@ -48,11 +68,11 @@ public class ESQueryBuilder {
 
     private FilterBuilder getFilter() {
         FilterBuilder filter;
-        if(this.filters.size() == 0) {
+        if(filters.isEmpty()) {
             // a FilteredQuery can have no Filter.
             filter = null;
         } else {
-            FilterBuilder array[] = this.filters.toArray(new FilterBuilder[this.filters.size()]);
+            FilterBuilder array[] = filters.toArray(new FilterBuilder[filters.size()]);
             filter = FilterBuilders.boolFilter().must(array);
         }
         return filter;
