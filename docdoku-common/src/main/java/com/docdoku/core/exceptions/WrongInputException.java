63c2d028d4bb9f23a3d4bdecfb21aa859eaf31f2
Elisabel Genereux <elisabel.genereux@docdoku.com>
2015-10-22 12:05:37 +0200

    added WrongInputException

---
+++ docdoku-common/src/main/java/com/docdoku/core/exceptions/WrongInputException.java
@@ -0,0 +1,47 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2015 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.docdoku.core.exceptions;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Elisabel Généreux
+ */
+public class WrongInputException extends ApplicationException {
+
+    public WrongInputException(String pMessage) {
+        super(pMessage);
+    }
+
+    public WrongInputException(Locale pLocale) {
+        this(pLocale, null);
+    }
+
+    public WrongInputException(Locale pLocale, Throwable pCause) {
+        super(pLocale, pCause);
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return getBundleDefaultMessage();
+    }
+}
