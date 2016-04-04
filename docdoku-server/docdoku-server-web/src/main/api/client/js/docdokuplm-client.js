634a1772ba193af43aed91532137b7c597e19826
morgan <morgan.guimard@docdoku.com>
2016-04-04 10:13:49 +0200

    Update JS API

--- docdoku-server/docdoku-server-web/src/main/api/client/js/docdokuplm-client.js
+++ docdoku-server/docdoku-server-web/src/main/api/client/js/docdokuplm-client.js
@@ -1,28 +1,46 @@
-window.DocdokuPLMClient = {
-    api:null,
-    loadAPI: function (callback){
+(function() {
 
-        if(DocdokuPLMClient.api){
-            return callback(DocdokuPLMClient.api);
-        }
+    window.DocdokuPLMClient = {
 
-        var src = '${server.contextRoot}/client/js/swagger-client.js';
-        var s = document.createElement('script');
-        s.setAttribute( 'src', src );
+        loadAPI: function (host, useRequire, callback){
 
-        s.onload=function(){
+            var swaggerClient = host+'${server.contextRoot}/client/js/swagger-client.js';
+            var swaggerDefinition = host+'${server.contextRoot}/client/swagger.json';
+
             var d = Date.now();
-            new SwaggerClient({
-                url: '${server.contextRoot}/client/swagger.json',
-                usePromise: true
-            }).then(function(api) {
-                window.DocdokuPLMClient.api = api;
-                console.log('Api loaded in ' + (Date.now() - d) + ' ms');
-                console.log(api);
-                callback(api);
-            });
-        };
 
-        document.body.appendChild(s);
-    }
-};
+            var onLoaded = function(api){
+                console.log('Api loaded in ' + (Date.now() - d) + ' ms');
+                callback(api);
+            };
+
+            if(useRequire && typeof require === 'function'){
+                require([swaggerClient],function(SwaggerAPI){
+                    new SwaggerAPI.SwaggerClient({
+                        url: swaggerDefinition,
+                        usePromise: true
+                    }).then(onLoaded);
+                });
+            }
+            else if(typeof window !== 'undefined'){
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+
+                s.onload=function(){
+                    s.onload = null;
+                    new SwaggerClient({
+                        url: swaggerDefinition,
+                        usePromise: true
+                    }).then(onLoaded);
+                };
+
+                (document.getElementsByTagName( "head" )[ 0 ]).appendChild( s );
+
+                s.src = swaggerClient;
+            } else {
+                console.error('Not supported');
+            }
+        }
+    };
+
+})();
