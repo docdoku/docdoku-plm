8c32a14f388588a5aa892e973ca38ac064891ff9
morgan <morgan.guimard@docdoku.com>
2016-04-21 14:32:37 +0200

    Update paths

--- docdoku-server/docdoku-server-web-front/tests/config.local.js
+++ docdoku-server/docdoku-server-web-front/tests/config.local.js
@@ -3,11 +3,11 @@
 /*
 * Before you modify this file, please run the following git command
 *
-*   git update-index --skip-worktree docdoku-server/docdoku-server-web/yo/tests/config.local.js
+*   git update-index --skip-worktree docdoku-server/docdoku-server-web-front/tests/config.local.js
 *
 * To revert :
 *
-*   git update-index --no-skip-worktree docdoku-server/docdoku-server-web/yo/tests/config.local.js
+*   git update-index --no-skip-worktree docdoku-server/docdoku-server-web-front/tests/config.local.js
 *
 * Don't commit any overrides of this module.
 */
