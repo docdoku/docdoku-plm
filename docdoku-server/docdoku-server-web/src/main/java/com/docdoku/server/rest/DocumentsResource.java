1ec0560bee64b74c0c35a1efe93abfe3a3a1f148
Florent Garin <florent.garin@docdoku.com>
2016-06-17 17:24:35 +0200

    fix issue on headers

--- docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/DocumentsResource.java
+++ docdoku-server/docdoku-server-web/src/main/java/com/docdoku/server/rest/DocumentsResource.java
@@ -1,4 +1,5 @@
-/* DocDoku, Professional Open Source
+/*
+ * DocDoku, Professional Open Source
  * Copyright 2006 - 2015 DocDoku SARL
  *
  * This file is part of DocDokuPLM.
@@ -8,14 +9,15 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * DocDokuPLM is distributed in the hope that it will be useful,  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of  
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
- * GNU Affero General Public License for more details.  
- *  
- * You should have received a copy of the GNU Affero General Public License  
- * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.  
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.docdoku.server.rest;
 
 import com.docdoku.core.configuration.DocumentConfigSpec;
