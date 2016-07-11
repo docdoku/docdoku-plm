2f0c3d8fe6caf01c7c50b0311962404966288e42
morgan <morgan.guimard@docdoku.com>
2016-07-11 09:54:52 +0200

    [API]  Missing header + doc

--- docdoku-api-java/src/main/java/com/docdoku/api/models/utils/LastIterationHelper.java
+++ docdoku-api-java/src/main/java/com/docdoku/api/models/utils/LastIterationHelper.java
@@ -1,10 +1,33 @@
+/*
+ * DocDoku, Professional Open Source
+ * Copyright 2006 - 2015 DocDoku SARL
+ *
+ * This file is part of DocDokuPLM.
+ *
+ * DocDokuPLM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DocDokuPLM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with DocDokuPLM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.docdoku.api.models.utils;
 
 import com.docdoku.api.models.DocumentIterationDTO;
 import com.docdoku.api.models.DocumentRevisionDTO;
 import com.docdoku.api.models.PartIterationDTO;
 import com.docdoku.api.models.PartRevisionDTO;
-
+/**
+ * This class helps to get last iteration from documents or parts
+ * @Author Morgan Guimard
+ */
 public class LastIterationHelper {
 
     public static DocumentIterationDTO getLastIteration(DocumentRevisionDTO documentRevisionDTO){
