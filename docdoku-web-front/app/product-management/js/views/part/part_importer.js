7c800ce40d0bc2dc44a871eb104871ac59c78d62
laurentlevan <laurent.levan@docdoku.com>
2016-07-15 16:20:49 +0200

    Made change to allow import preview server side.
    (cherry picked from commit 9af79f59e140e5d5a568512bc9a3933dccfb7c64)

--- docdoku-web-front/app/product-management/js/views/part/part_importer.js
+++ docdoku-web-front/app/product-management/js/views/part/part_importer.js
@@ -7,26 +7,31 @@ define([
     'common-objects/models/file/attached_file',
     'common-objects/views/file/file',
     'common-objects/views/alert',
+    'text!templates/part/part_import_modal.html',
     'text!templates/part/part_import_form.html',
     'common-objects/views/part/import_status_view'
-], function (Backbone, Mustache, unorm, ModalView, AttachedFile, FileView, AlertView, template, ImportStatusView) {
+], function (Backbone, Mustache, unorm, ModalView, AttachedFile, FileView, AlertView, modalTemplate, template, ImportStatusView) {
     'use strict';
     var PartImportView = Backbone.View.extend({
 
         template: template,
+        modalTemplate: modalTemplate,
 
         tagName: 'div',
         className: 'attachedFiles idle',
 
-        events:{
-            'click form button.cancel-upload-btn':'cancelButtonClicked',
-            'change form input.upload-btn':'fileSelectHandler',
-            'dragover .droppable':'fileDragHover',
-            'dragleave .droppable':'fileDragHover',
-            'drop .droppable':'fileDropHandler',
-            'click .import-button':'formSubmit',
-            'click #auto_checkout_part':'changeAutoCheckout',
-            'hidden .importer-view':'onHidden'
+        events: {
+            'click form button.cancel-upload-btn': 'cancelButtonClicked',
+            'change form input.upload-btn': 'fileSelectHandler',
+            'dragover .droppable': 'fileDragHover',
+            'dragleave .droppable': 'fileDragHover',
+            'drop .droppable': 'fileDropHandler',
+            'click .import-button': 'formSubmit',
+            'click #auto_checkout_part': 'changeAutoCheckout',
+            'hidden .importer-view': 'onHidden',
+            'click .import-preview-button': 'showPreview',
+            'click .back-button': 'backToForm',
+            'hidden.bs.modal .modal.importer-view':'deleteImportStatus'
         },
 
         initialize: function () {
@@ -44,6 +49,9 @@ define([
             }, false);
 
             this.$el.on('remove', this.removeSubviews);
+
+            this.importForm = true;
+            this.importPreview = false;
         },
 
         // cancel event and hover styling
@@ -80,7 +88,14 @@ define([
 
         render: function () {
 
-            this.$el.html(Mustache.render(template, {
+            this.$el.html(Mustache.render(modalTemplate, {i18n: App.config.i18n}));
+            this.$el.find('#import-contain').append(Mustache.render(template, {
+                importForm: this.importForm,
+                importPreview: this.importPreview,
+                checkout: this.autocheckout,
+                checkin: this.autocheckin,
+                permissive: this.permissive,
+                revisionNote: this.revisionNote,
                 i18n: App.config.i18n
             }));
             this.bindDomElements();
@@ -103,9 +118,37 @@ define([
 
         },
 
+        rerender: function () {
+            this.$el.find('#import-contain').html(Mustache.render(template, {
+                importForm: this.importForm,
+                importPreview: this.importPreview,
+                checkout: this.autocheckout,
+                checkin: this.autocheckin,
+                permissive: this.permissive,
+                revisionNote: this.revisionNote,
+                partList:this.partCheckoutList,
+                i18n: App.config.i18n
+            }));
+
+            //this.delegateEvents();
+            this.bindDomElements();
+
+            if(this.autocheckout) { this.checkboxAutoCheckout.prop('checked', true); }
+            this.$('#revision_text_part').val(this.revisionNote);
+            if(this.autocheckout){
+                this.checkboxAutoCheckout.prop('checked', true);
+                this.checkboxAutoCheckin.prop('disabled', false);
+            }
+            else {
+                this.checkboxAutoCheckin.prop('disabled', true);
+            }
+            if(this.autocheckin){ this.checkboxAutoCheckin.prop('checked', true); }
+            if(this.permissive){ this.$('#permissive_update_part').prop('checked',true); }
+        },
+
         addOneFile: function (attachedFile) {
             this.filedisplay.html('<li>' + attachedFile.getShortName() + '</li>');
-            this.$('.import-button').removeAttr('disabled');
+            this.$('.import-preview-button').removeAttr('disabled');
         },
 
         bindDomElements: function () {
@@ -130,25 +173,48 @@ define([
                 this.checkboxAutoCheckin.is('checked', false);
             }
         },
-        formSubmit: function () {
+
+        showPreview: function () {
 
             this.clearNotifications();
 
-            var baseUrl = App.config.contextPath + '/api/workspaces/' + App.config.workspaceId + '/parts/import';
+            this.autocheckin = this.checkboxAutoCheckin.is(':checked');
+            this.autocheckout = this.checkboxAutoCheckout.is(':checked');
+            this.permissive = this.$('#permissive_update_part').is(':checked');
+            this.revisionNote = this.$('#revision_text_part').val().trim();
 
-            var autocheckin = this.checkboxAutoCheckin.is(':checked');
-            var autocheckout = this.checkboxAutoCheckout.is(':checked');
-            var permissive = this.$('#permissive_update_part').is(':checked');
-            var revisionNote = this.$('#revision_text_part').val().trim();
+            if (this.file) {
+
+                if (this.autocheckout) {
+                    var previewUrl = App.config.contextPath + '/api/workspaces/' + App.config.workspaceId + '/parts/import';
+
+                    $.get(previewUrl, {upload : this.file }, function(data){
+                        this.partCheckoutList = data;
+                    });
+
+                }
+
+                this.importForm = false;
+                this.importPreview = true;
+                this.rerender();
+
+            }
+        },
+        formSubmit: function () {
+
+            var baseUrl = App.config.contextPath + '/api/workspaces/' + App.config.workspaceId + '/parts/import';
 
             if (this.file) {
 
                 var params = {
-                    autoCheckout: autocheckout,
-                    autoCheckin: autocheckin,
-                    permissiveUpdate: permissive,
-                    revisionNote: revisionNote
+                    autoCheckout: this.autocheckout,
+                    autoCheckin: this.autocheckin,
+                    permissiveUpdate: this.permissive,
+                    revisionNote: this.revisionNote
                 };
+
+                this.deleteImportStatus();
+                this.deleteImportStatus();
 
                 var importUrl = baseUrl + '?' + $.param(params);
 
@@ -159,40 +225,41 @@ define([
                 formData.append('upload', this.file);
                 xhr.send(formData);
 
-            }else if(!this.file){
+            } else if (!this.file) {
                 this.printNotifications('error', App.config.i18n.NO_FILE_TO_IMPORT);
             }
 
+            this.backToForm();
             return false;
         },
 
-        fetchImports:function(){
+        fetchImports: function () {
             var _this = this;
             this.removeSubviews();
             _this.$('.import-status-views').empty();
 
             var url = App.config.contextPath + '/api/workspaces/' + App.config.workspaceId + '/parts/import';
-            $.get(url).then(function(imports){
-                _.each(imports,function(pImport){
-                    var view = new ImportStatusView({model:pImport}).render();
+            $.get(url).then(function (imports) {
+                _.each(imports, function (pImport) {
+                    var view = new ImportStatusView({model: pImport}).render();
                     _this.importStatusViews.push(view);
                     _this.$('.import-status-views').append(view.$el);
                 });
             });
         },
 
-        printNotifications: function(type,message) {
+        printNotifications: function (type, message) {
             this.notifications.append(new AlertView({
                 type: type,
                 message: message
             }).render().$el);
         },
 
-        clearNotifications: function() {
+        clearNotifications: function () {
             this.notifications.text('');
         },
 
-        removeSubviews: function(){
+        removeSubviews: function () {
             _(this.importStatusViews).invoke('remove');
             this.importStatusViews = [];
         },
@@ -207,6 +274,19 @@ define([
 
         onHidden: function () {
             this.remove();
+        },
+
+        backToForm: function () {
+            this.importPreview = false;
+            this.importForm = true;
+            this.rerender();
+            this.loadNewFile(this.file);
+        },
+
+        deleteImportStatus: function (){
+            _.each(this.importStatusViews, function(importSV){
+                importSV.deleteImport();
+            });
         }
 
     });
