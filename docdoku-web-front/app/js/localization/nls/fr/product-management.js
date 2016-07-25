5ed240eaf6a4f973462d876f2a631e9f81aba2be
Elisabel Genereux <elisabel.genereux@docdoku.com>
2016-07-25 14:06:33 +0200

    added question marks

--- docdoku-web-front/app/js/localization/nls/fr/product-management.js
+++ docdoku-web-front/app/js/localization/nls/fr/product-management.js
@@ -1,11 +1,11 @@
 /*global define*/
 define({
-    CONFIRM_DELETE_BASELINE:'Voulez-vous supprimer les baselines sélectionnées',
-    CONFIRM_DELETE_PART:'Voulez-vous supprimer les articles sélectionnés',
-    CONFIRM_DELETE_PART_TEMPLATE:'Voulez-vous supprimer les modèles d\'articles sélectionnés',
-    CONFIRM_DELETE_PRODUCT:'Voulez-vous supprimer les produits sélectionnés',
-    CONFIRM_DELETE_PRODUCT_INSTANCE:'Voulez-vous supprimer les exemplaires de produits sélectionnés',
-    CONFIRM_DELETE_CONFIGURATION:'Voulez-vous supprimer les configurations sélectionnées',
+    CONFIRM_DELETE_BASELINE:'Voulez-vous supprimer les baselines sélectionnées ?',
+    CONFIRM_DELETE_PART:'Voulez-vous supprimer les articles sélectionnés ?',
+    CONFIRM_DELETE_PART_TEMPLATE:'Voulez-vous supprimer les modèles d\'articles sélectionnés ?',
+    CONFIRM_DELETE_PRODUCT:'Voulez-vous supprimer les produits sélectionnés ?',
+    CONFIRM_DELETE_PRODUCT_INSTANCE:'Voulez-vous supprimer les exemplaires de produits sélectionnés ?',
+    CONFIRM_DELETE_CONFIGURATION:'Voulez-vous supprimer les configurations sélectionnées ?',
     CREATE_BASELINE_LATEST: 'Créer la baseline avec les derniers articles',
     CREATE_BASELINE_RELEASED: 'Créer la baseline avec les derniers articles finalisés',
     CREATE_PRODUCT_BEFORE_BASELINE:'Vous devez avoir créé au moins un produit pour créer des baselines',
