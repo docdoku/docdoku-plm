339f1ecd9451455f0d8b27ac4c03084a12c621a1
morgan <morgan.guimard@docdoku.com>
2016-07-26 12:31:14 +0200

    [DPLM]  Find last modifications in folders

--- docdoku-dplm/app/js/home/home.js
+++ docdoku-dplm/app/js/home/home.js
@@ -11,11 +11,15 @@
             });
         })
 
-        .controller('HomeCtrl',function ($scope, $mdMedia, ConfigurationService, FolderService, WorkspaceService) {
+        .controller('HomeCtrl',function ($scope, $mdMedia, ConfigurationService, FolderService, WorkspaceService,RepositoryService) {
             $scope.$mdMedia = $mdMedia;
             $scope.configuration = ConfigurationService.configuration;
             $scope.folders = FolderService.folders;
             $scope.workspaces = WorkspaceService.workspaces;
+            $scope.modifiedFiles = {};
+            angular.forEach($scope.folders,function(folder){
+                $scope.modifiedFiles[folder.path] = RepositoryService.getFilesWithModifications(folder.path);
+            });
         })
 
         .controller('FolderDetailsController',function($scope,FolderService){
