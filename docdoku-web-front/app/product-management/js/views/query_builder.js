c803d3f97ccb4a0ee9d8ba76387d4749f3d7eb5f
Elisabel Genereux <elisabel.genereux@docdoku.com>
2016-08-05 10:38:37 +0200

    [FRONT] display where values of saved query in query builder

--- docdoku-web-front/app/product-management/js/views/query_builder.js
+++ docdoku-web-front/app/product-management/js/views/query_builder.js
@@ -121,9 +121,20 @@ define([
             if(e.target.value){
                 var query = _.findWhere(this.queries,{id: parseInt(e.target.value,10)});
                 if (query.queryRule) {
-                    if (query.queryRule.rules.length === 0){
+                    if (query.queryRule.rules.length === 0) {
                         this.$where.queryBuilder('reset');
-                    }else{
+                    } else {
+                        if (query.queryRule && query.queryRule.rules) {
+                            var rules = query.queryRule.rules;
+                            for (var i=0; i<rules.length; i++) {
+                                if (rules[i].values.length == 1) {
+                                    rules[i].value = rules[i].values[0];
+                                } else {
+                                    rules[i].value = rules[i].values;
+                                }
+                                rules[i].values = undefined;
+                            }
+                        }
                         this.$where.queryBuilder('setRules', query.queryRule);
                     }
                 }
