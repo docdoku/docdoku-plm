b824d5ad06b9655a558f82238d41a45e91ba857a
Florent Garin <florent.garin@docdoku.com>
2016-09-13 15:44:59 +0200

    Fixed type No coworkers to No coworker

--- docdoku-web-front/app/js/modules/coworkers-access-module/views/coworkers_access_module_view.js
+++ docdoku-web-front/app/js/modules/coworkers-access-module/views/coworkers_access_module_view.js
@@ -24,7 +24,7 @@ define([
 
                 if(!coWorkers.length){
                     var menuUrl = App.config.contextPath+'/workspace-management/';
-                    $ul.append('<li><i>&nbsp;'+App.config.i18n.NO_COWORKERS+'</i></li>');
+                    $ul.append('<li><i>&nbsp;'+App.config.i18n.NO_COWORKER+'</i></li>');
                     $ul.append('<li><a href="'+menuUrl+'"><i class="fa fa-cog"></i> '+App.config.i18n.WORKSPACES_ADMINISTRATION+'</a></li>');
                     return;
                 }
