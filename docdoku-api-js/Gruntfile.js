7564141f66e425856f95ad02271d3c5ca7decd5c
morgan <morgan.guimard@docdoku.com>
2016-12-07 12:12:50 +0100

    [API-JS] Online doc

--- docdoku-api-js/Gruntfile.js
+++ docdoku-api-js/Gruntfile.js
@@ -21,11 +21,36 @@ module.exports = function (grunt) {
                     'target/docdoku-plm-api/browser/docdoku-plm-api.js': ['target/docdoku-plm-api/npm/src/index.js', 'exports/exports.js']
                 }
             }
+        },
+        md2html: {
+            doc: {
+                options: {},
+                files: [{
+                    expand: true,
+                    cwd: 'target/docdoku-plm-api/npm/docs/',
+                    src: ['*.md'],
+                    dest: 'target/docdoku-plm-api/site/docs/',
+                    ext: '.html'
+                },{
+                    src: ['target/docdoku-plm-api/npm/README.md'],
+                    dest: 'target/docdoku-plm-api/site/index.html'
+                }]
+            }
+        },
+        replace:{
+            mdLinks:{
+                src: ['target/docdoku-plm-api/site/**/*.html'],
+                overwrite: true,
+                replacements: [{
+                    from: /\.md/g,
+                    to: '.html'
+                }]
+            }
         }
 
     });
 
-    grunt.registerTask('build', ['clean', 'browserify:dist']);
+    grunt.registerTask('build', ['clean', 'browserify:dist', 'md2html:doc', 'replace:mdLinks']);
 };
 
 
