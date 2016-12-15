f4fd899c86c6a5742b795e5d43f901c963eddb76
morgan <morgan.guimard@docdoku.com>
2016-12-15 16:04:11 +0100

    [CLI] Review recursive download/checkout code

--- docdoku-cli/src/main/java/com/docdoku/cli/commands/parts/PartCheckOutCommand.java
+++ docdoku-cli/src/main/java/com/docdoku/cli/commands/parts/PartCheckOutCommand.java
@@ -40,106 +40,110 @@ import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 /**
- *
  * @author Florent Garin
  */
 public class PartCheckOutCommand extends BaseCommandLine {
 
-    @Option(metaVar = "<revision>", name="-r", aliases = "--revision", usage="specify revision of the part to check out ('A', 'B'...); if not specified the part identity (number and revision) corresponding to the cad file will be selected")
+    @Option(metaVar = "<revision>", name = "-r", aliases = "--revision", usage = "specify revision of the part to check out ('A', 'B'...); if not specified the part identity (number and revision) corresponding to the cad file will be selected")
     private String revision;
 
     @Option(metaVar = "<partnumber>", name = "-o", aliases = "--part", usage = "the part number of the part to check out; if not specified choose the part corresponding to the cad file")
     private String partNumber;
 
-    @Argument(metaVar = "[<cadfile> | <dir>]", index=0, usage = "specify the cad file of the part to check out or the path where cad files are stored (default is working directory)")
+    @Argument(metaVar = "[<cadfile> | <dir>]", index = 0, usage = "specify the cad file of the part to check out or the path where cad files are stored (default is working directory)")
     private File path = new File(System.getProperty("user.dir"));
 
-    @Option(name="-n", aliases = "--no-download", usage="do not download the native cad file of the part if any")
+    @Option(name = "-n", aliases = "--no-download", usage = "do not download the native cad file of the part if any")
     private boolean noDownload;
 
-    @Option(name="-f", aliases = "--force", usage="overwrite existing files even if they have been modified locally")
+    @Option(name = "-f", aliases = "--force", usage = "overwrite existing files even if they have been modified locally")
     private boolean force;
 
-    @Option(name="-R", aliases = "--recursive", usage="execute the command through the product structure hierarchy")
+    @Option(name = "-R", aliases = "--recursive", usage = "execute the command through the product structure hierarchy")
     private boolean recursive;
 
-    @Option(name="-w", aliases = "--workspace", required = true, metaVar = "<workspace>", usage="workspace on which operations occur")
+    @Option(name = "-w", aliases = "--workspace", required = true, metaVar = "<workspace>", usage = "workspace on which operations occur")
     protected String workspace;
 
-    @Option(name="-b", aliases = "--baseline", metaVar = "<baseline>", usage="baseline to filter")
-    protected int baselineId;
+    @Option(name = "-b", aliases = "--baseline", metaVar = "<baseline>", usage = "baseline to filter")
+    protected Integer baselineId;
 
     @Override
     public void execImpl() throws Exception {
-        if(partNumber==null || revision==null){
+        if (partNumber == null || revision == null) {
             loadMetadata();
         }
-
-        String strRevision = revision==null?null:revision;
-        checkoutPart(partNumber,strRevision);
+        Set<String> alreadyProcessed = new HashSet<>();
+        checkoutPart(partNumber, revision, alreadyProcessed);
     }
 
     private void loadMetadata() throws IOException {
-        if(path.isDirectory()){
-            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified1",user));
+        if (path.isDirectory()) {
+            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified1", user));
         }
         MetaDirectoryManager meta = new MetaDirectoryManager(path.getParentFile());
         String filePath = path.getAbsolutePath();
         partNumber = meta.getPartNumber(filePath);
         String strRevision = meta.getRevision(filePath);
-        if(partNumber==null || strRevision==null){
-            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified2",user));
+        if (partNumber == null || strRevision == null) {
+            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified2", user));
         }
         revision = strRevision;
         //once partNumber and revision have been inferred, set path to folder where files are stored
         //in order to implement perform the rest of the treatment
-        path=path.getParentFile();
+        path = path.getParentFile();
     }
 
-    private void checkoutPart(String pPartNumber, String pRevision) throws IOException, ApiException, LoginException, NoSuchAlgorithmException {
+    private void checkoutPart(String pPartNumber, String pRevision, Set<String> alreadyProcessed) throws IOException, ApiException, LoginException, NoSuchAlgorithmException {
+
+        if (alreadyProcessed.contains(pPartNumber)) {
+            return;
+        }
 
         PartsApi partsApi = new PartsApi(client);
         PartApi partApi = new PartApi(client);
 
         Locale locale = new AccountsManager().getUserLocale(user);
 
-        //PartMaster pm = productS.getPartMaster(new PartMasterKey(workspace, pPartNumber));
         PartRevisionDTO pr;
         PartIterationDTO pi;
 
-        output.printInfo(LangHelper.getLocalizedMessage("CheckingOutPart",locale)+ " : "+ pPartNumber);
+        output.printInfo(LangHelper.getLocalizedMessage("CheckingOutPart", locale) + " : " + pPartNumber);
 
-        if(baselineId != 0){
-            pi = partsApi.filterPartMasterInBaseline(workspace, pPartNumber,String.valueOf(baselineId));
-            pr = partsApi.getPartRevision(workspace, pPartNumber, pi.getVersion());
-        }else {
+        if (pRevision != null) {
             pr = partsApi.getPartRevision(workspace, pPartNumber, pRevision);
+            pi = LastIterationHelper.getLastIteration(pr);
+        } else if (baselineId != null) {
+            pi = partsApi.filterPartMasterInBaseline(workspace, pPartNumber, String.valueOf(baselineId));
+            pr = partsApi.getPartRevision(workspace, pPartNumber, pi.getVersion());
+        } else {
+            pr = partsApi.getLatestPartRevision(workspace, pPartNumber);
             pi = LastIterationHelper.getLastIteration(pr);
         }
 
-        if(pr.getCheckOutUser() == null) {
-            try{
-                pr = partApi.checkOut(workspace, pPartNumber, pr.getVersion());
-                pi = LastIterationHelper.getLastIteration(pr);
-            }catch (Exception e){
-                output.printException(e);
-            }
+        if (pr.getCheckOutUser() == null) {
+            pr = partApi.checkOut(workspace, pPartNumber, pr.getVersion());
+            pi = LastIterationHelper.getLastIteration(pr);
         }
 
         BinaryResourceDTO nativeCADFile = pi.getNativeCADFile();
 
-        if(nativeCADFile!=null && !noDownload){
-            FileHelper fh = new FileHelper(user,password,output,locale);
+        if (nativeCADFile != null && !noDownload) {
+            FileHelper fh = new FileHelper(user, password, output, locale);
             fh.downloadNativeCADFile(getServerURL(), path, workspace, pPartNumber, pr, pi, force);
         }
 
-        if(recursive){
+        alreadyProcessed.add(pPartNumber);
 
-            for(PartUsageLinkDTO link:pi.getComponents()){
-                checkoutPart(link.getComponent().getNumber(), null);
+        if (recursive) {
+            for (PartUsageLinkDTO link : pi.getComponents()) {
+                String linkNumber = link.getComponent().getNumber();
+                checkoutPart(linkNumber, null, alreadyProcessed);
             }
         }
 
@@ -147,6 +151,6 @@ public class PartCheckOutCommand extends BaseCommandLine {
 
     @Override
     public String getDescription() throws IOException {
-        return LangHelper.getLocalizedMessage("PartCheckOutCommandDescription",user);
+        return LangHelper.getLocalizedMessage("PartCheckOutCommandDescription", user);
     }
 }
--- docdoku-cli/src/main/java/com/docdoku/cli/commands/parts/PartGetCommand.java
+++ docdoku-cli/src/main/java/com/docdoku/cli/commands/parts/PartGetCommand.java
@@ -25,9 +25,8 @@ import com.docdoku.api.models.BinaryResourceDTO;
 import com.docdoku.api.models.PartIterationDTO;
 import com.docdoku.api.models.PartRevisionDTO;
 import com.docdoku.api.models.PartUsageLinkDTO;
-import com.docdoku.api.services.PartApi;
-import com.docdoku.api.services.PartsApi;
 import com.docdoku.api.models.utils.LastIterationHelper;
+import com.docdoku.api.services.PartsApi;
 import com.docdoku.cli.commands.BaseCommandLine;
 import com.docdoku.cli.helpers.AccountsManager;
 import com.docdoku.cli.helpers.FileHelper;
@@ -40,100 +39,107 @@ import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- *
  * @author Florent Garin
  */
 public class PartGetCommand extends BaseCommandLine {
 
 
-    @Option(metaVar = "<revision>", name="-r", aliases = "--revision", usage="specify revision of the part to retrieve ('A', 'B'...); default is the latest")
+    @Option(metaVar = "<revision>", name = "-r", aliases = "--revision", usage = "specify revision of the part to retrieve ('A', 'B'...); default is the latest")
     private String revision;
 
-    @Option(name="-i", aliases = "--iteration", metaVar = "<iteration>", usage="specify iteration of the part to retrieve ('1','2', '24'...); default is the latest")
-    private int iteration;
+    @Option(name = "-i", aliases = "--iteration", metaVar = "<iteration>", usage = "specify iteration of the part to retrieve ('1','2', '24'...); default is the latest")
+    private Integer iteration;
 
     @Option(metaVar = "<partnumber>", name = "-o", aliases = "--part", usage = "the part number of the part to fetch; if not specified choose the part corresponding to the cad file")
     private String partNumber;
 
-    @Argument(metaVar = "[<cadfile> | <dir>]", index=0, usage = "specify the cad file of the part to fetch or the path where cad files are stored (default is working directory)")
+    @Argument(metaVar = "[<cadfile> | <dir>]", index = 0, usage = "specify the cad file of the part to fetch or the path where cad files are stored (default is working directory)")
     private File path = new File(System.getProperty("user.dir"));
 
-    @Option(name="-f", aliases = "--force", usage="overwrite existing files even if they have been modified locally")
+    @Option(name = "-f", aliases = "--force", usage = "overwrite existing files even if they have been modified locally")
     private boolean force;
 
-    @Option(name="-R", aliases = "--recursive", usage="execute the command through the product structure hierarchy")
+    @Option(name = "-R", aliases = "--recursive", usage = "execute the command through the product structure hierarchy")
     private boolean recursive;
 
-    @Option(name="-w", aliases = "--workspace", required = true, metaVar = "<workspace>", usage="workspace on which operations occur")
+    @Option(name = "-w", aliases = "--workspace", required = true, metaVar = "<workspace>", usage = "workspace on which operations occur")
     protected String workspace;
 
-    @Option(name="-b", aliases = "--baseline", metaVar = "<baseline>", usage="baseline to filter")
-    protected int baselineId;
+    @Option(name = "-b", aliases = "--baseline", metaVar = "<baseline>", usage = "baseline to filter")
+    protected Integer baselineId;
 
     @Override
     public void execImpl() throws Exception {
 
-        if(partNumber==null){
+        if (partNumber == null) {
             loadMetadata();
         }
 
-        String strRevision = revision==null?null:revision;
-
-
-        getPart(partNumber, strRevision, iteration);
+        Set<String> alreadyProcessed = new HashSet<>();
+        getPart(partNumber, revision, iteration, alreadyProcessed);
     }
 
     private void loadMetadata() throws IOException {
-        if(path.isDirectory()){
-            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified1",user));
+        if (path.isDirectory()) {
+            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified1", user));
         }
         MetaDirectoryManager meta = new MetaDirectoryManager(path.getParentFile());
         String filePath = path.getAbsolutePath();
         partNumber = meta.getPartNumber(filePath);
         String strRevision = meta.getRevision(filePath);
-        if(partNumber==null || strRevision==null){
-            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified2",user));
+        if (partNumber == null || strRevision == null) {
+            throw new IllegalArgumentException(LangHelper.getLocalizedMessage("PartNumberOrRevisionNotSpecified2", user));
         }
         revision = strRevision;
         //The part is inferred from the cad file, hence fetch the fresh (latest) iteration
-        iteration=0;
+        iteration = 0;
         //once partNumber and revision have been inferred, set path to folder where files are stored
         //in order to implement perform the rest of the treatment
-        path=path.getParentFile();
+        path = path.getParentFile();
     }
 
-    private void getPart(String pPartNumber, String pRevision, int pIteration) throws IOException, ApiException, LoginException, NoSuchAlgorithmException {
+    private void getPart(String pPartNumber, String pRevision, Integer pIteration, Set<String> alreadyProcessed) throws IOException, ApiException, LoginException, NoSuchAlgorithmException {
 
-        // TODO may break => unused parameter pIteration ?? should review algorithm and command specs
+        if (alreadyProcessed.contains(pPartNumber)) {
+            return;
+        }
 
         PartsApi partsApi = new PartsApi(client);
-        PartApi partApi = new PartApi(client);
 
         PartRevisionDTO pr;
         PartIterationDTO pi;
 
-        if(baselineId != 0){
-            pi = partsApi.filterPartMasterInBaseline(workspace, pPartNumber, String.valueOf(baselineId));
-            pr = partApi.getPartRevision(workspace,pPartNumber,pi.getVersion());
-        }else {
+        if (pRevision != null) {
             pr = partsApi.getPartRevision(workspace, pPartNumber, pRevision);
+            pi = pIteration != null ? pr.getPartIterations().get(pIteration - 1) : LastIterationHelper.getLastIteration(pr);
+        } else if (baselineId != null) {
+            pi = partsApi.filterPartMasterInBaseline(workspace, pPartNumber, String.valueOf(baselineId));
+            pr = partsApi.getPartRevision(workspace, pPartNumber, pi.getVersion());
+        } else {
+            pr = partsApi.getLatestPartRevision(workspace, pPartNumber);
             pi = LastIterationHelper.getLastIteration(pr);
         }
 
         BinaryResourceDTO nativeCADFile = pi.getNativeCADFile();
 
-        if(nativeCADFile!=null){
-            FileHelper fh = new FileHelper(user,password,output,new AccountsManager().getUserLocale(user));
+        if (nativeCADFile != null) {
+            FileHelper fh = new FileHelper(user, password, output, new AccountsManager().getUserLocale(user));
             fh.downloadNativeCADFile(getServerURL(), path, workspace, pPartNumber, pr, pi, force);
-        }else{
-            output.printInfo(LangHelper.getLocalizedMessage("NoFileForPart",user) + " : "  + pPartNumber + " " + pr.getVersion() + "." + pi.getIteration() + " (" + workspace + ")");
+        } else {
+            output.printInfo(LangHelper.getLocalizedMessage("NoFileForPart", user) + " : " + pPartNumber +
+                    " " + pr.getVersion() + "." + pi.getIteration() + " (" + workspace + ")");
         }
 
-        if(recursive){
-            for(PartUsageLinkDTO link:pi.getComponents()){
-                getPart(link.getComponent().getNumber(),null,0);
+        alreadyProcessed.add(pPartNumber);
+
+        if (recursive) {
+            for (PartUsageLinkDTO link : pi.getComponents()) {
+                String linkNumber = link.getComponent().getNumber();
+                getPart(linkNumber, null, null, alreadyProcessed);
             }
         }
 
@@ -141,6 +147,6 @@ public class PartGetCommand extends BaseCommandLine {
 
     @Override
     public String getDescription() throws IOException {
-        return LangHelper.getLocalizedMessage("PartGetCommandDescription",user);
+        return LangHelper.getLocalizedMessage("PartGetCommandDescription", user);
     }
 }
