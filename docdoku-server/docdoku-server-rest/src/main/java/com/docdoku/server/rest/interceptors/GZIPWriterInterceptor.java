5779b08da7afbfd537b9c43050ec4b3a009e26e7
morgan <morgan.guimard@docdoku.com>
2017-01-20 15:00:52 +0100

    Add custom header to bypass Accept-Encoding header

--- docdoku-server/docdoku-server-rest/src/main/java/com/docdoku/server/rest/interceptors/GZIPWriterInterceptor.java
+++ docdoku-server/docdoku-server-rest/src/main/java/com/docdoku/server/rest/interceptors/GZIPWriterInterceptor.java
@@ -20,6 +20,8 @@
 
 package com.docdoku.server.rest.interceptors;
 
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 import javax.ws.rs.ext.WriterInterceptor;
@@ -32,18 +34,28 @@ import java.util.zip.GZIPOutputStream;
 public class GZIPWriterInterceptor implements WriterInterceptor {
     private static final int DEFAULT_BUFFER_SIZE = 4096;
 
+    @Context
+    private HttpHeaders requestHeaders;
 
     @Override
     public void aroundWriteTo(WriterInterceptorContext context) throws IOException {
-        MultivaluedMap<String, Object> headers = context.getHeaders();
-        Object rangeHeader = headers.getFirst("Content-Range");
+
+
+        MultivaluedMap<String, Object> responseHeaders = context.getHeaders();
+        Object rangeHeader = responseHeaders.getFirst("Content-Range");
+
+        // Use a custom header here
+        // Some clients needs to know the content length in response headers in order to display a loading state
+        // Browsers don't let programmers to change the default "Accept-Encoding" header, then we use a custom one.
+        String acceptEncoding = requestHeaders.getHeaderString("x-accept-encoding");
 
         GZIPOutputStream gzipOutputStream = null;
 
-
-        if (rangeHeader == null) {
-            headers.add("Content-Encoding", "gzip");
-            headers.remove("Content-Length");
+        if (acceptEncoding != null && acceptEncoding.equals("identity")) {
+            responseHeaders.add("Content-Encoding", "identity");
+        } else if (rangeHeader == null) {
+            responseHeaders.add("Content-Encoding", "gzip");
+            responseHeaders.remove("Content-Length");
             gzipOutputStream = new GZIPOutputStream(context.getOutputStream(), DEFAULT_BUFFER_SIZE);
             context.setOutputStream(gzipOutputStream);
         }
