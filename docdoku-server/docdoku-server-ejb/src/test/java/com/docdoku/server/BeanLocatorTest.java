6853835348df7f94b3915bba74eb5355aff813df
morgan <morgan.guimard@docdoku.com>
2017-01-26 10:00:19 +0100

    Rename sun-ejb-jar.xml to glassfish-ejb-jar.xml - Add "disable-nonportable-jndi-names=true" property to plugin modules

--- docdoku-server/docdoku-server-ejb/src/test/java/com/docdoku/server/BeanLocatorTest.java
+++ docdoku-server/docdoku-server-ejb/src/test/java/com/docdoku/server/BeanLocatorTest.java
@@ -1,18 +1,16 @@
 package com.docdoku.server;
 
-import java.util.Collections;
-import java.util.Hashtable;
-import java.util.List;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-
+import com.docdoku.server.converters.CADConverter;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import com.docdoku.server.converters.CADConverter;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
 
 public class BeanLocatorTest {
 
@@ -20,30 +18,30 @@ public class BeanLocatorTest {
 
     @BeforeClass
     public static void setup() throws Exception {
-	ctx = new InitialContext(new Hashtable<>(Collections.singletonMap(Context.INITIAL_CONTEXT_FACTORY,
-		"org.osjava.sj.memory.MemoryContextFactory")));
-	CADConverter converter1 = Mockito.mock(CADConverter.class);
-	Mockito.when(converter1.canConvertToOBJ("format")).thenReturn(true);
-	CADConverter converter2 = Mockito.mock(CADConverter.class);
-	Mockito.when(converter2.canConvertToOBJ("format")).thenReturn(true);
-	ctx.createSubcontext("java:global");
-	ctx.createSubcontext("java:global/application");
-	ctx.createSubcontext("java:global/application/module");
-	ctx.bind("java:global/application/module/c1Bean!com.docdoku.server.converters.CADConverter", converter1);
-	ctx.bind("java:global/application/module/c1Bean", converter1);
-	ctx.bind("java:global/application/module/c2Bean", converter2);
-	ctx.bind("java:global/application/module/c2Bean!com.docdoku.server.converters.CADConverter", converter2);
+        ctx = new InitialContext(new Hashtable<>(Collections.singletonMap(Context.INITIAL_CONTEXT_FACTORY,
+                "org.osjava.sj.memory.MemoryContextFactory")));
+        CADConverter converter1 = Mockito.mock(CADConverter.class);
+        Mockito.when(converter1.canConvertToOBJ("format")).thenReturn(true);
+        CADConverter converter2 = Mockito.mock(CADConverter.class);
+        Mockito.when(converter2.canConvertToOBJ("format")).thenReturn(true);
+        ctx.createSubcontext("java:global");
+        ctx.createSubcontext("java:global/application");
+        ctx.createSubcontext("java:global/application/module");
+        ctx.bind("java:global/application/module/c1Bean!com.docdoku.server.converters.CADConverter", converter1);
+        ctx.bind("java:global/application/module/c1Bean", converter1);
+        ctx.bind("java:global/application/module/c2Bean", converter2);
+        ctx.bind("java:global/application/module/c2Bean!com.docdoku.server.converters.CADConverter", converter2);
     }
-    
+
     BeanLocator locator = new BeanLocator();
 
     @Test
     public void testSearch() throws Exception {
-	List<CADConverter> converters = locator.search(CADConverter.class, ctx);
+        List<CADConverter> converters = locator.search(CADConverter.class, ctx);
 
-	Assert.assertEquals(2, converters.size());
-	for (CADConverter c : converters) {
-	    Assert.assertTrue(c.canConvertToOBJ("format"));
-	}
+        Assert.assertEquals(2, converters.size());
+        for (CADConverter c : converters) {
+            Assert.assertTrue(c.canConvertToOBJ("format"));
+        }
     }
 }
--- docdoku-server/docdoku-server-ejb/src/test/java/com/docdoku/server/ConverterBeanTest.java
+++ docdoku-server/docdoku-server-ejb/src/test/java/com/docdoku/server/ConverterBeanTest.java
@@ -1,30 +1,5 @@
 package com.docdoku.server;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.net.URI;
-import java.nio.file.Paths;
-import java.util.Arrays;
-
-import javax.persistence.EntityManager;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
 import com.docdoku.core.common.BinaryResource;
 import com.docdoku.core.product.Geometry;
 import com.docdoku.core.product.PartIteration;
@@ -33,6 +8,26 @@ import com.docdoku.core.services.IBinaryStorageManagerLocal;
 import com.docdoku.core.services.IProductManagerLocal;
 import com.docdoku.server.converters.CADConverter;
 import com.docdoku.server.converters.ConversionResult;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.persistence.EntityManager;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConverterBeanTest {
@@ -75,74 +70,74 @@ public class ConverterBeanTest {
 
     @Before
     public void setup() throws Exception {
-	when(cadBinRes.getName()).thenReturn("foo.dae");
-	when(em.find(PartIteration.class, ipk)).thenReturn(partIter);
-	when(storage.getBinaryResourceInputStream(cadBinRes))
-	.thenReturn(new ByteArrayInputStream("fake content".getBytes()));
+        when(cadBinRes.getName()).thenReturn("foo.dae");
+        when(em.find(PartIteration.class, ipk)).thenReturn(partIter);
+        when(storage.getBinaryResourceInputStream(cadBinRes))
+                .thenReturn(new ByteArrayInputStream("fake content".getBytes()));
 
-	when(product.saveGeometryInPartIteration(eq(ipk), anyString(), anyInt(), anyLong(), any(double[].class)))
-	.thenReturn(lod);
-	when(storage.getBinaryResourceOutputStream(lod)).thenReturn(new ByteArrayOutputStream());
-	
-	when(product.saveFileInPartIteration(eq(ipk), anyString(), eq("attachedfiles"), anyLong()))
-	.thenReturn(attachedFile, attachedFile);
-	when(storage.getBinaryResourceOutputStream(attachedFile)).thenReturn(new ByteArrayOutputStream());
-	
-	when(conv.canConvertToOBJ("dae")).thenReturn(true);
+        when(product.saveGeometryInPartIteration(eq(ipk), anyString(), anyInt(), anyLong(), any(double[].class)))
+                .thenReturn(lod);
+        when(storage.getBinaryResourceOutputStream(lod)).thenReturn(new ByteArrayOutputStream());
 
-	when(result.getConvertedFile()).thenReturn(Paths.get("src/test/resources/fake.obj"));
-	when(result.getMaterials()).thenReturn(Arrays.asList(Paths.get("src/test/resources/fake.obj.1.mtl"),
-		Paths.get("src/test/resources/fake.obj.2.mtl")));
+        when(product.saveFileInPartIteration(eq(ipk), anyString(), eq("attachedfiles"), anyLong()))
+                .thenReturn(attachedFile, attachedFile);
+        when(storage.getBinaryResourceOutputStream(attachedFile)).thenReturn(new ByteArrayOutputStream());
 
-	when(conv.convert(any(URI.class), any(URI.class))).thenReturn(result);
+        when(conv.canConvertToOBJ("dae")).thenReturn(true);
 
-	when(locator.search(CADConverter.class)).thenReturn(Arrays.asList(conv));
-	
-	bean.init();	
+        when(result.getConvertedFile()).thenReturn(Paths.get("src/test/resources/fake.obj"));
+        when(result.getMaterials()).thenReturn(Arrays.asList(Paths.get("src/test/resources/fake.obj.1.mtl"),
+                Paths.get("src/test/resources/fake.obj.2.mtl")));
+
+        when(conv.convert(any(URI.class), any(URI.class))).thenReturn(result);
+
+        when(locator.search(CADConverter.class)).thenReturn(Arrays.asList(conv));
+
+        bean.init();
     }
 
     @Test
     public void testNominalConvert() throws Exception {
-	// * test *
-	bean.convertCADFileToOBJ(ipk, cadBinRes);
-	verify(locator).search(CADConverter.class);
-	verify(conv).canConvertToOBJ("dae");
-	verify(conv).convert(any(URI.class), any(URI.class));
-	verify(product).saveGeometryInPartIteration(eq(ipk), anyString(), anyInt(), anyLong(), any(double[].class));
-	verify(storage).getBinaryResourceOutputStream(lod);
-	verify(product, times(2)).saveFileInPartIteration(eq(ipk), anyString(), eq("attachedfiles"), anyLong());
-	verify(storage, times(2)).getBinaryResourceOutputStream(attachedFile);
+        // * test *
+        bean.convertCADFileToOBJ(ipk, cadBinRes);
+        verify(locator).search(CADConverter.class);
+        verify(conv).canConvertToOBJ("dae");
+        verify(conv).convert(any(URI.class), any(URI.class));
+        verify(product).saveGeometryInPartIteration(eq(ipk), anyString(), anyInt(), anyLong(), any(double[].class));
+        verify(storage).getBinaryResourceOutputStream(lod);
+        verify(product, times(2)).saveFileInPartIteration(eq(ipk), anyString(), eq("attachedfiles"), anyLong());
+        verify(storage, times(2)).getBinaryResourceOutputStream(attachedFile);
     }
 
     @Test
     public void testNoConverter() throws Exception {
-	// * setup *
-	when(cadBinRes.getName()).thenReturn("foo.unknown");
+        // * setup *
+        when(cadBinRes.getName()).thenReturn("foo.unknown");
 
-	// * test *
-	bean.convertCADFileToOBJ(ipk, cadBinRes);
+        // * test *
+        bean.convertCADFileToOBJ(ipk, cadBinRes);
 
-	verify(locator).search(CADConverter.class);
-	verify(conv).canConvertToOBJ("unknown");
-	verify(conv, never()).convert(any(), any());
-	verify(product, never()).saveGeometryInPartIteration(any(), anyString(), anyInt(), anyLong(), any());
-	verify(product, never()).saveFileInPartIteration(any(), anyString(), anyString(), anyLong());
-	verify(storage, never()).getBinaryResourceOutputStream(any());
+        verify(locator).search(CADConverter.class);
+        verify(conv).canConvertToOBJ("unknown");
+        verify(conv, never()).convert(any(), any());
+        verify(product, never()).saveGeometryInPartIteration(any(), anyString(), anyInt(), anyLong(), any());
+        verify(product, never()).saveFileInPartIteration(any(), anyString(), anyString(), anyLong());
+        verify(storage, never()).getBinaryResourceOutputStream(any());
     }
 
     @Test
     public void testBrokenConvert() throws Exception {
-	// * setup *
-	when(conv.convert(any(URI.class), any(URI.class))).thenThrow(new CADConverter.ConversionException("error"));
-	
-	// * test *
-	bean.convertCADFileToOBJ(ipk, cadBinRes);
-	
-	verify(conv).canConvertToOBJ("dae");
-	verify(conv).convert(any(URI.class), any(URI.class));
-	verify(product, never()).saveGeometryInPartIteration(any(), anyString(), anyInt(), anyLong(), any());
-	verify(product, never()).saveFileInPartIteration(any(), anyString(), anyString(), anyLong());
-	verify(storage, never()).getBinaryResourceOutputStream(any());
+        // * setup *
+        when(conv.convert(any(URI.class), any(URI.class))).thenThrow(new CADConverter.ConversionException("error"));
+
+        // * test *
+        bean.convertCADFileToOBJ(ipk, cadBinRes);
+
+        verify(conv).canConvertToOBJ("dae");
+        verify(conv).convert(any(URI.class), any(URI.class));
+        verify(product, never()).saveGeometryInPartIteration(any(), anyString(), anyInt(), anyLong(), any());
+        verify(product, never()).saveFileInPartIteration(any(), anyString(), anyString(), anyLong());
+        verify(storage, never()).getBinaryResourceOutputStream(any());
     }
 
 }
