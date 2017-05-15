5e2959d193dc7781a27a95e093bea6d92c30544c
Morgan Guimard <morgan.guimard@docdoku.com>
2017-05-15 10:09:14 +0200

    Remove DPLM from source control - gitlab migration - Restore docdoku-api-js build

--- docdoku-dplm/app/modules.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('dplm.templates', []);
-    angular.module('dplm.pages', []);
-    angular.module('dplm.filters', []);
-    angular.module('dplm.dialogs', []);
-    angular.module('dplm.services', []);
-    angular.module('dplm.directives', []);
-})();
