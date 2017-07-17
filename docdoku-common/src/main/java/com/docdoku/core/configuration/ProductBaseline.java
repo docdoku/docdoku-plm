0794d4cfd0757d372c5810e8f646374e61357ae5
Morgan Guimard <morgan.guimard@docdoku.com>
2017-07-17 13:20:00 +0200

    RevisionStatus : Rewrite non valid query

--- docdoku-common/src/main/java/com/docdoku/core/configuration/ProductBaseline.java
+++ docdoku-common/src/main/java/com/docdoku/core/configuration/ProductBaseline.java
@@ -47,7 +47,7 @@ import java.util.*;
 @NamedQueries({
         @NamedQuery(name = "ProductBaseline.findByConfigurationItemId", query = "SELECT b FROM ProductBaseline b WHERE b.configurationItem.id = :ciId AND b.configurationItem.workspace.id = :workspaceId"),
         @NamedQuery(name = "ProductBaseline.getBaselinesForPartRevision", query = "SELECT b FROM ProductBaseline b WHERE b.partCollection IN (SELECT bl.partCollection FROM BaselinedPart bl WHERE bl.targetPart.partRevision = :partRevision)"),
-        @NamedQuery(name = "ProductBaseline.findObsoletePartRevisions", query = "SELECT pr FROM PartRevision pr JOIN ProductBaseline pb JOIN BaselinedPart bp WHERE pb = :productBaseline AND pb.partCollection.id = bp.partCollection.id AND bp.targetPart.partRevision.status = com.docdoku.core.product.PartRevisionStatus.OBSOLETE AND bp.targetPart.partRevision.partMasterWorkspaceId = :workspaceId")
+        @NamedQuery(name = "ProductBaseline.findObsoletePartRevisions", query = "SELECT pr FROM PartRevision pr JOIN ProductBaseline pb JOIN BaselinedPart bp WHERE pb = :productBaseline AND pb.partCollection.id = bp.partCollection.id AND bp.targetPart.partRevision.status = com.docdoku.core.meta.RevisionStatus.OBSOLETE AND bp.targetPart.partRevision.partMasterWorkspaceId = :workspaceId")
 
 })
 public class ProductBaseline implements Serializable {
