d48503606099fb6a4fa6ebbc34b4f536ab0b7320
Morgan Guimard <morgan.guimard@docdoku.com>
2017-09-28 12:21:36 +0200

    Remove common, server and i18n modules. Replaced by eplmp git submodule

--- docdoku-server/docdoku-server-ejb/src/main/java/com/docdoku/server/hooks/WebhookRunner.java
+++
@@ -1,7 +0,0 @@
-package com.docdoku.server.hooks;
-
-import com.docdoku.core.hooks.Webhook;
-
-public interface WebhookRunner {
-    void run(Webhook webhook, String login, String email, String name, String subject, String content);
-}
